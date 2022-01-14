af13225fa4fb3614acf64ed198b3612c3a48b2fd
triszt4n <trisztanpiller@gmail.com>
2022-01-14 13:40:44 +0100

    Simple fixes on components

--- src/main/client/src/components/@layout/Footer.tsx
+++ src/main/client/src/components/@layout/Footer.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react'
-import { Box } from '@chakra-ui/react'
+import { Box, Container, useColorModeValue } from '@chakra-ui/react'
 import { BUGREPORT_URL, KIRDEV_URL } from '../../utils/configurations'
 
 type FooterProps = {}
 
 export const Footer: React.FC<FooterProps> = ({}) => (
-  <Box>
-    {KIRDEV_URL} | {BUGREPORT_URL}
+  <Box borderTopWidth={1} borderStyle="solid" borderColor={useColorModeValue('gray.200', 'gray.700')}>
+    <Container maxW="6xl" py={4}>
+      {KIRDEV_URL} | {BUGREPORT_URL}
+    </Container>
   </Box>
 )
