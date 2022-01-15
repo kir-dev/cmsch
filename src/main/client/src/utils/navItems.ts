21001dfc2e390a39b84992f7ef26643fe6b349db
Bálint Berente <balint.berente@gmail.com>
2022-01-15 13:58:22 +0100

    Added navItems

--- src/main/client/src/utils/navItems.ts
+++ src/main/client/src/utils/navItems.ts
@@ -2,33 +2,15 @@ import { NavItem } from '../types/NavItem'
 
 export const NAV_ITEMS: Array<NavItem> = [
   {
-    label: 'Blog',
-    href: '/blog',
-    children: [
-      {
-        label: 'Legújabb posztok',
-        href: '/blog'
-      },
-      {
-        label: 'Archívum',
-        href: '/archive'
-      }
-    ]
+    label: 'Főoldal',
+    href: '/'
   },
   {
-    label: 'Rólunk',
-    href: '/about'
+    label: 'Reszortok',
+    href: '/reszortok'
   },
   {
-    label: 'Csapatunk',
-    href: '/members'
-  },
-  {
-    label: 'Projektjeink',
-    href: '/projects'
-  },
-  {
-    label: 'Tanfolyam',
-    href: '/courses'
+    label: 'Körök',
+    href: '/korok'
   }
 ]
