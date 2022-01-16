80f6e479fd9d41d3c1d36a6f8abcf647fd49fa7c
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-01-16 22:43:42 +0100

    Add QR scanner component

--- src/main/client/src/types/dto/token.ts
+++ src/main/client/src/types/dto/token.ts
@@ -2,3 +2,27 @@ export interface TokenDTO {
   title: string
   type: string
 }
+
+export enum ScanStatus {
+  SCANNED,
+  ALREADY_SCANNED,
+  WRONG
+}
+
+export interface ScanResponseDTO {
+  title?: string
+  status: ScanStatus
+}
+
+export enum ScanViewState {
+  Scanning,
+  Loading,
+  Error,
+  Success
+}
+
+export interface ScanView {
+  state: ScanViewState
+  response?: ScanResponseDTO
+  errorMessage?: string
+}
