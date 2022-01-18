019a0336e9822b035aa49c6ea818d86f91a263c1
Bálint Berente <balint.berente@gmail.com>
2022-01-18 16:09:29 +0100

    First version of the Main Site layout

--- src/main/client/src/components/@pages/Home.tsx
+++ src/main/client/src/components/@pages/Home.tsx
@@ -1,27 +1,60 @@
-import { Alert, AlertIcon, Button, ButtonGroup, Heading } from '@chakra-ui/react'
-import { PlusSquareIcon } from '@chakra-ui/icons'
+import { Alert, AlertIcon, ButtonGroup, Heading, Stack } from '@chakra-ui/react'
 import { Paragraph } from '../@commons/Basics'
 import { Page } from '../@layout/Page'
 import React from 'react'
+import { LinkButton } from '../@commons/LinkButton'
+import { ImageCarousel } from '../@commons/ImageCarousel'
+import { Schedule } from '../@commons/Schedule'
+import { EVENTS } from '../../content/events'
 
 export const Home: React.FC = () => {
   return (
     <Page>
-      <Heading>Üdvözlünk a GólyaKörTe portálon</Heading>
-      <Alert status="warning" variant="left-accent">
-        <AlertIcon />
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit nisi, facilisis id iaculis eu, pretium sed diam. Sed sit amet
-        nibh non felis venenatis gravida. Aenean semper accumsan ante varius gravida.
-      </Alert>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam velit nisi, facilisis id iaculis eu, pretium sed diam. Sed sit amet
-        nibh non felis venenatis gravida. Aenean semper accumsan ante varius gravida.
+      <Heading size="3xl" textAlign="center">
+        Üdvözlünk a{' '}
+        <Heading as="span" color="brand.500" size="3xl">
+          GólyaKörTe
+        </Heading>{' '}
+        portálon
+      </Heading>
+      <Paragraph marginTop={20} color="gray.500" textAlign="center" maxW={800} marginX="auto">
+        Elérkezett a GólyaKörTe! Ezen az oldalon tájékozódhatsz az eseményről. A rendezvény alatt feladatokat tudsz megoldani nyereményekért
+        és tanköri aláírásért!
       </Paragraph>
-      <ButtonGroup>
-        <Button colorScheme="brand" leftIcon={<PlusSquareIcon />}>
-          Click me
-        </Button>
+      <Stack justifyContent="center" marginTop={20} direction="column" alignItems="center">
+        <LinkButton href="/korok" colorScheme="brand" size="lg">
+          Körök listája
+        </LinkButton>
+        <LinkButton href="/reszortok" colorScheme="brand" size="lg" variant="ghost">
+          Reszortok listája
+        </LinkButton>
+      </Stack>
+      <Heading as="h2" size="lg" marginTop={20} textAlign="center">
+        Feladatok
+      </Heading>
+      <Paragraph textAlign="center">Három típusú feladatot tudtok teljesíteni. Ezekhez AuthSch fiók használata szükséges!</Paragraph>
+      <ButtonGroup marginTop={10} justifyContent="center">
+        <LinkButton href="#" variant="outline" colorScheme="brand">
+          Riddle
+        </LinkButton>
+        <LinkButton href="/bucketlist" variant="outline" colorScheme="brand">
+          Bucketlist
+        </LinkButton>
+        <LinkButton href="#" variant="outline" colorScheme="brand">
+          QR-kódok
+        </LinkButton>
       </ButtonGroup>
+      <Heading as="h2" size="lg" textAlign="center" marginTop={20}>
+        Események
+      </Heading>
+      <Alert marginTop={10} variant="left-accent" width="fit-content" marginX="auto">
+        <AlertIcon />A változás jogát fenntartjuk! Kísérd figyelemmel az oldal tetején megjelenő értesítéseket!
+      </Alert>
+      <Schedule events={EVENTS} />
+      <Heading as="h2" size="lg" textAlign="center" marginTop={20}>
+        Térképek
+      </Heading>
+      <ImageCarousel images={['/img/maps/bmei.png', '/img/maps/sch.png', '/img/maps/fnt.png']} />
     </Page>
   )
 }
