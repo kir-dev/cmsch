b9e3970793702ada105f61fc9138f60964e18106
Bálint Berente <balint.berente@gmail.com>
2022-01-18 16:09:49 +0100

    Created a Grid based timetable.

---
+++ src/main/client/src/components/@commons/Schedule.tsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import { Grid, GridItem, Text } from '@chakra-ui/react'
+import { useColorModeValue } from '@chakra-ui/system'
+import { Event } from '../../types/Event'
+
+type ScheduleProps = {
+  events: Event[]
+}
+export const Schedule: React.FC<ScheduleProps> = ({ events }) => {
+  return (
+    <Grid templateColumns="repeat(2, auto)" gap={10} marginTop={10}>
+      {events.map((event) => (
+        <EventDisplay event={event} />
+      ))}
+    </Grid>
+  )
+}
+
+type EventDisplayProps = {
+  event: Event
+}
+
+const EventDisplay: React.FC<EventDisplayProps> = ({ event }) => {
+  return (
+    <>
+      <GridItem textAlign="right">
+        <Text fontSize="2xl" color={useColorModeValue('brand.700', 'brand.300')}>
+          {event.start}-{event.end}
+        </Text>
+      </GridItem>
+      <GridItem>
+        <Text fontSize="2xl">{event.name}</Text>
+        <Text as="i" fontSize="xl" color="gray.500">
+          {event.location}
+        </Text>
+      </GridItem>
+    </>
+  )
+}
