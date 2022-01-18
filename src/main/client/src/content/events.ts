264a81d3c9d04f629529a0fe1ac509bcb40d1fb3
Bálint Berente <balint.berente@gmail.com>
2022-01-18 16:08:32 +0100

    Events content

---
+++ src/main/client/src/content/events.ts
@@ -0,0 +1,16 @@
+import { Event } from '../types/Event'
+
+export const EVENTS: Event[] = [
+  {
+    start: '12:00',
+    end: '13:00',
+    name: 'Epic megnyitó',
+    location: 'I épület'
+  },
+  {
+    start: '13:00',
+    end: '14:00',
+    name: 'Epic Kir-Dev bemutató',
+    location: 'I épület'
+  }
+]
