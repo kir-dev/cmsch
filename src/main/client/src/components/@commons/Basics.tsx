03f6f0cd8b6aa9b9556b3a2af4240aed5fc0020c
Bálint Berente <balint.berente@gmail.com>
2022-01-18 16:09:05 +0100

    Added TextParams

--- src/main/client/src/components/@commons/Basics.tsx
+++ src/main/client/src/components/@commons/Basics.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { Text } from '@chakra-ui/react'
+import { Text, TextProps } from '@chakra-ui/react'
 
-export const Paragraph: React.FC = ({ children, ...props }) => {
+export const Paragraph: React.FC<TextProps> = ({ children, textAlign, ...props }) => {
   return (
-    <Text marginTop={5} {...props} textAlign="justify">
+    <Text marginTop={5} {...props} textAlign={textAlign || 'justify'}>
       {children}
     </Text>
   )
