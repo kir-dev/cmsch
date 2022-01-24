8212fc3fefb735c7e262f64fce607d27c856ef5e
triszt4n <trisztanpiller@gmail.com>
2022-01-24 11:56:21 +0100

    CMSCH #132 New colors

--- src/main/client/src/utils/customTheme.ts
+++ src/main/client/src/utils/customTheme.ts
@@ -13,15 +13,43 @@ const customTheme = extendTheme({
   },
   colors: {
     brand: {
-      100: '#d4f3e7',
-      200: '#a8e7ce',
-      300: '#7ddcb6',
-      400: '#51d09d',
-      500: '#26c485',
-      600: '#1e9d6a',
-      700: '#177650',
-      800: '#0f4e35',
-      900: '#08271b'
+      // generated here: https://maketintsandshades.com/#80C500
+      50: '#f2f9e6',
+      100: '#d9eeb3',
+      200: '#c0e280',
+      300: '#a6d64d',
+      400: '#8dcb1a',
+      500: '#80c500',
+      600: '#669e00',
+      700: '#4d7600',
+      800: '#334f00',
+      900: '#1a2700'
+    },
+    brand2: {
+      // generated here: https://maketintsandshades.com/#FAA916
+      50: '#fff6e8',
+      100: '#fee5b9',
+      200: '#fdd48b',
+      300: '#fcc35c',
+      400: '#fbb22d',
+      500: '#faa916',
+      600: '#c88712',
+      700: '#96650d',
+      800: '#644409',
+      900: '#322204'
+    },
+    gray: {
+      // generated here: https://maketintsandshades.com/#78716c
+      50: '#f2f1f0',
+      100: '#d7d4d3',
+      200: '#bcb8b6',
+      300: '#a19c98',
+      400: '#867f7b',
+      500: '#78716c',
+      600: '#605a56',
+      700: '#484441',
+      800: '#302d2b',
+      900: '#1d1c1b'
     }
   },
   components: {
