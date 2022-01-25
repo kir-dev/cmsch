d2325898026429f36d9e31690660a9448625ffa3
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-01-25 19:12:40 +0100

    Improve stamp component colors

--- src/main/client/src/components/@commons/StampComponent.tsx
+++ src/main/client/src/components/@commons/StampComponent.tsx
@@ -1,5 +1,6 @@
 import { Box, Center, Flex, Icon, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useColorModeValue } from '@chakra-ui/system'
 import { FaStamp } from 'react-icons/fa'
 
 interface StampComponentProps {
@@ -8,14 +9,17 @@ interface StampComponentProps {
 }
 
 export const StampComponent: React.FC<StampComponentProps> = ({ title }: StampComponentProps) => {
+  const backgroundBase = useColorModeValue('gray.100', 'gray.700')
+  const stampCorner = useColorModeValue('gray.200', 'gray.600')
+
   return (
-    <Box maxW="md" minW={['100%', 'md']} borderRadius="lg" boxShadow="lg" bg="brand.100">
+    <Box maxW="md" minW={['100%', 'md']} borderRadius="lg" bg={backgroundBase}>
       <Flex>
-        <Center bg="brand.300" padding="2" borderStartRadius="lg">
-          <Icon as={FaStamp} boxSize="2em" fontSize="3xl" color="black" />
+        <Center bg={stampCorner} padding="2" borderStartRadius="lg">
+          <Icon as={FaStamp} boxSize="2em" fontSize="3xl" color="brand.500" />
         </Center>
         <Center width="100%" paddingStart="3" textAlign="center">
-          <Text color="black" fontSize="xl" fontWeight="bold">
+          <Text fontSize="xl" fontWeight="bold">
             {title}
           </Text>
         </Center>
