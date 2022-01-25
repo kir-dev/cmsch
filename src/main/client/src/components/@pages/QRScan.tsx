05304610227a853f2848050ea20cb1f47d24a123
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-01-25 20:11:20 +0100

    use common loading component

--- src/main/client/src/components/@pages/QRScan.tsx
+++ src/main/client/src/components/@pages/QRScan.tsx
@@ -21,6 +21,7 @@ import QRreader from 'react-qr-reader'
 import { useNavigate } from 'react-router-dom'
 import { ScanResponseDTO, ScanStatus } from 'types/dto/token'
 import { API_BASE_URL } from 'utils/configurations'
+import { Loading } from 'utils/Loading'
 import { useServiceContext } from '../../utils/useServiceContext'
 
 enum ScanViewState {
@@ -73,32 +74,29 @@ export const QRScan: React.FC = (props) => {
     setState({ state: ScanViewState.Scanning, response: undefined })
   }
 
+  if (state.state == ScanViewState.Loading) return <Loading timeout={0} />
+
   return (
     <Page {...props} loginRequired>
       <Heading>Scanneld be a QR kódot</Heading>
       {state.state == ScanViewState.Scanning && <QRreader delay={300} onError={handleError} onScan={handleScan} />}
-      {state.state == ScanViewState.Loading && (
-        <Center>
-          <CircularProgress isIndeterminate color="brand.300" size="120px" />
-        </Center>
-      )}
+
       {state.state == ScanViewState.Success && (
         <Fade in={state.state == ScanViewState.Success}>
           <QRScanResultComponent response={state.response || { status: ScanStatus.WRONG }} />
         </Fade>
       )}
-      {state.state !== ScanViewState.Loading && (
-        <ButtonGroup alignSelf="center">
-          <LinkButton marginTop="3" leftIcon={<FaArrowLeft />} size="lg" href="/qr">
-            Vissza
-          </LinkButton>
-          {state.state !== ScanViewState.Scanning && (
-            <Button marginTop="3" colorScheme="brand" leftIcon={<FaQrcode />} onClick={resetButtonHandler} size="lg">
-              Új QR scannelése
-            </Button>
-          )}
-        </ButtonGroup>
-      )}
+
+      <ButtonGroup alignSelf="center">
+        <LinkButton marginTop="3" leftIcon={<FaArrowLeft />} size="lg" href="/qr">
+          Vissza
+        </LinkButton>
+        {state.state !== ScanViewState.Scanning && (
+          <Button marginTop="3" colorScheme="brand" leftIcon={<FaQrcode />} onClick={resetButtonHandler} size="lg">
+            Új QR scannelése
+          </Button>
+        )}
+      </ButtonGroup>
     </Page>
   )
 }
