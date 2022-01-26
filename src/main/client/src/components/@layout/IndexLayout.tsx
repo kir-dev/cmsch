c8a75fe86c170b9e1e91fda22feb625a15269079
Bálint Berente <balint.berente@gmail.com>
2022-01-26 19:37:49 +0100

    Added ScrollToTop

--- src/main/client/src/components/@layout/IndexLayout.tsx
+++ src/main/client/src/components/@layout/IndexLayout.tsx
@@ -3,6 +3,7 @@ import { Warning } from 'components/@commons/Warning'
 import * as React from 'react'
 import { Footer } from './Footer'
 import { Navbar } from './navigation/Navbar'
+import ScrollToTop from '../../utils/ScrollToTop'
 
 type IndexLayoutProps = {
   background?: string
@@ -11,6 +12,7 @@ type IndexLayoutProps = {
 export const IndexLayout: React.FC<IndexLayoutProps> = ({ background, children }) => (
   <>
     <Flex direction="column" minHeight="100vh">
+      <ScrollToTop />
       <Navbar />
       <Box background={background} flex={1} pb={20}>
         <Warning />
