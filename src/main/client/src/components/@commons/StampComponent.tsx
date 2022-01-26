ab9d2489dec702f3958c6d4230885dc733855bd9
Bucsy Benjámin <bucsy.benjamin@simonyi.bme.hu>
2022-01-26 18:10:10 +0100

    handle extra tokens
    Use one state instead of individual ones, and only count "default" tokens when counting progress

--- src/main/client/src/components/@commons/StampComponent.tsx
+++ src/main/client/src/components/@commons/StampComponent.tsx
@@ -1,22 +1,25 @@
 import { Box, Center, Flex, Icon, Text } from '@chakra-ui/react'
 import React from 'react'
 import { useColorModeValue } from '@chakra-ui/system'
-import { FaStamp } from 'react-icons/fa'
+import { FaRocket, FaStamp } from 'react-icons/fa'
+import { IconType } from 'react-icons/lib'
 
 interface StampComponentProps {
   title?: string
   type: string
 }
 
-export const StampComponent: React.FC<StampComponentProps> = ({ title }: StampComponentProps) => {
+export const StampComponent: React.FC<StampComponentProps> = ({ title, type }: StampComponentProps) => {
   const backgroundBase = useColorModeValue('gray.100', 'gray.700')
   const stampCorner = useColorModeValue('gray.200', 'gray.600')
+
+  const icon: IconType = type === 'default' ? FaStamp : FaRocket
 
   return (
     <Box maxW="md" minW={['100%', 'md']} borderRadius="lg" bg={backgroundBase}>
       <Flex>
         <Center bg={stampCorner} padding="2" borderStartRadius="lg">
-          <Icon as={FaStamp} boxSize="2em" fontSize="3xl" color="brand.500" />
+          <Icon as={icon} boxSize="2em" fontSize="3xl" color="brand.500" />
         </Center>
         <Center width="100%" paddingStart="3" textAlign="center">
           <Text fontSize="xl" fontWeight="bold">
