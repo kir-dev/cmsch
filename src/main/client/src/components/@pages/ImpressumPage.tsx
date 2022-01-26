6f15ddea764bfb1ad1d9ad170b811ea6a696fd2a
Bálint Berente <balint.berente@gmail.com>
2022-01-26 17:07:55 +0100

    Small fixes

--- src/main/client/src/components/@pages/ImpressumPage.tsx
+++ src/main/client/src/components/@pages/ImpressumPage.tsx
@@ -1,8 +1,10 @@
-import { Heading, HStack, Link, Tag, Text, Image, Wrap, WrapItem, Center, Flex, useColorModeValue } from '@chakra-ui/react'
+import { Flex, Heading, HStack, Image, Link, Tag, Text, Wrap, WrapItem } from '@chakra-ui/react'
 import { Paragraph } from 'components/@commons/Basics'
 import { Page } from 'components/@layout/Page'
 import { DEVS } from 'content/devs'
-import { GITHUB_ORG_URL, KIRDEV_ORANGE, KIRDEV_URL } from 'utils/configurations'
+import { GITHUB_ORG_URL, KIRDEV_URL } from 'utils/configurations'
+import { useColorModeValue } from '@chakra-ui/system'
+import customTheme from '../../utils/customTheme'
 
 type ImpressumPageProps = {}
 
@@ -12,7 +14,7 @@ export const ImpressumPage: React.FC<ImpressumPageProps> = () => {
       <Heading>Impresszum</Heading>
       <Paragraph>
         A Gólyakörte 2022 weblapját az SSSL megbízásából a{' '}
-        <Link isExternal color={KIRDEV_ORANGE} href={KIRDEV_URL}>
+        <Link isExternal color={customTheme.colors.kirDev} href={KIRDEV_URL}>
           kir-dev
         </Link>{' '}
         <Text as="i">speed-run munkacsoport</Text> készítette.
@@ -22,30 +24,28 @@ export const ImpressumPage: React.FC<ImpressumPageProps> = () => {
       </Heading>
       <Wrap justify="center">
         {DEVS.map((dev) => (
-          <WrapItem border="2px" borderColor={useColorModeValue(KIRDEV_ORANGE, KIRDEV_ORANGE)} key={dev.name}>
-            <Center w="20rem" h="20rem">
-              <Flex direction="column" align="center">
-                <Text fontSize="2xl">{dev.name}</Text>
-                <Image src={dev.img} h="15rem" fallbackSrc="/img/communities/kirdev.svg" />
-                <HStack spacing={2} my={2}>
-                  {dev.tags.map((tag) => (
-                    <Tag size={'md'} variant="solid" fontWeight="bold" color="white" bgColor={KIRDEV_ORANGE} key={tag}>
-                      {tag}
-                    </Tag>
-                  ))}
-                </HStack>
-              </Flex>
-            </Center>
+          <WrapItem border="2px" borderColor={useColorModeValue('gray.200', 'gray.700')} borderRadius="md" key={dev.name}>
+            <Flex direction="column" align="center" w="20rem" h="20rem">
+              <Text fontSize="2xl">{dev.name}</Text>
+              <Image src={dev.img} h="15rem" fallbackSrc="/img/communities/kirdev.svg" />
+              <HStack spacing={2} my={2}>
+                {dev.tags.map((tag) => (
+                  <Tag size={'md'} variant="solid" fontWeight="bold" color="white" bgColor={customTheme.colors.kirDev} key={tag}>
+                    {tag}
+                  </Tag>
+                ))}
+              </HStack>
+            </Flex>
           </WrapItem>
         ))}
       </Wrap>
       <Paragraph>
         Felhasznált technológiák: Kotlin, Spring-boot, Typescript és React. Mint ahogy az összes többi projektünk, ez is{' '}
-        <Link isExternal color={KIRDEV_ORANGE} href={GITHUB_ORG_URL}>
+        <Link isExternal color={customTheme.colors.kirDev} href={GITHUB_ORG_URL}>
           nyílt forráskódú
         </Link>
         . Ha kérdésed van vagy érdekel a többi projektünk is, látogass el az{' '}
-        <Link isExternal color={KIRDEV_ORANGE} href={KIRDEV_URL}>
+        <Link isExternal color={customTheme.colors.kirDev} href={KIRDEV_URL}>
           oldalunkra
         </Link>{' '}
         vagy keress fel minket a kir-dev standnál!
