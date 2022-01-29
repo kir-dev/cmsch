309580d5ef7f6b165b3b8a0267f5b5aed1f6ad5d
Gerviba <gerviba@sch.bme.hu>
2022-01-29 23:46:14 +0100

    CMSCH-178 Add realtime configs to spring config

--- src/main/kotlin/hu/bme/sch/cmsch/controller/api/MainController.kt
+++ src/main/kotlin/hu/bme/sch/cmsch/controller/api/MainController.kt
@@ -39,7 +39,7 @@ class MainController(
 
     @ResponseBody
     @GetMapping("/version")
-    fun version(): String = "2.3.0"
+    fun version(): String = "2.4.0"
 
     @JsonView(Preview::class)
 //    @GetMapping("/news")
