22cb8aa2ee0ecccbfa70da6fc712fa4d545ca0bc
Bálint Berente <balint.berente@gmail.com>
2022-02-07 23:30:38 +0100

    Created frontend page for group selection

--- src/main/client/src/utils/AuthContext.tsx
+++ src/main/client/src/utils/AuthContext.tsx
@@ -11,19 +11,24 @@ const CookieKeys = {
 
 export type AuthContextType = {
   isLoggedIn: boolean
+  profile: ProfileDTO | undefined
   login: () => void
   logout: () => void
+  updateProfile: () => void
 }
 
 export const AuthContext = createContext<AuthContextType>({
   isLoggedIn: Cookies.get(CookieKeys.LOGGED_IN) === 'true',
+  profile: undefined,
   login: () => {},
-  logout: () => {}
+  logout: () => {},
+  updateProfile: () => {}
 })
 
 export const AuthProvider: React.FC = ({ children }) => {
   const { throwError } = useServiceContext()
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(Cookies.get(CookieKeys.LOGGED_IN) === 'true')
+  const [profile, setProfile] = useState<ProfileDTO>()
 
   const login = () => {
     location.href = '/control/login'
@@ -33,18 +38,24 @@ export const AuthProvider: React.FC = ({ children }) => {
     location.href = '/control/logout'
   }
 
-  useEffect(() => {
+  const updateProfile = () => {
     if (isLoggedIn)
       axios
         .get<ProfileDTO>(`${API_BASE_URL}/api/profile`)
         .then((res) => {
           if (typeof res.data !== 'object') logout()
+          setProfile(res.data)
         })
         .catch(() => {
           throwError('Újra be kell jelentkezned!', { toast: true, toastStatus: 'warning', toHomePage: true })
           setIsLoggedIn(false)
+          setProfile(undefined)
         })
+  }
+
+  useEffect(() => {
+    updateProfile()
   }, [isLoggedIn])
 
-  return <AuthContext.Provider value={{ isLoggedIn, login, logout }}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={{ isLoggedIn, profile, login, logout, updateProfile }}>{children}</AuthContext.Provider>
 }
