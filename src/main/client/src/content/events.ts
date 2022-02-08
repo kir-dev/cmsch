649f486c376bcd2c1aabf4e6ab2d4cddff5a121c
Sámuel Fekete <feketesamu@gmail.com>
2022-02-08 12:32:04 +0100

    events

--- src/main/client/src/content/events.ts
+++ src/main/client/src/content/events.ts
@@ -2,15 +2,33 @@ import { Event } from '../types/Event'
 
 export const EVENTS: Event[] = [
   {
-    start: '12:00',
-    end: '13:00',
-    name: 'Epic megnyitó',
+    start: '9:30',
+    end: '14:00',
+    name: 'Körök bemutatkozása',
     location: 'I épület'
   },
   {
-    start: '13:00',
-    end: '14:00',
-    name: 'Epic Kir-Dev bemutató',
-    location: 'I épület'
+    start: '14:00',
+    end: '15:30',
+    name: 'Ebédszünet és szint beszélgetések',
+    location: 'Gólyaszinti liftközök'
+  },
+  {
+    start: '15:30',
+    end: '20:00',
+    name: 'Körök bemutatkozása',
+    location: 'Schönherz'
+  },
+  {
+    start: '20:00',
+    end: '21:00',
+    name: 'Nevtanári beszélgetések',
+    location: 'Gólyaszinti liftközök'
+  },
+  {
+    start: '21:00',
+    end: '4:00',
+    name: 'Nyereményjáték hirdetés',
+    location: 'Schönherz'
   }
 ]
