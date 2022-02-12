b05032f2e9d5cd042f1a789cdea7f9f9301373e9
Bálint Berente <balint.berente@gmail.com>
2022-02-12 15:39:35 +0100

    Added hash links to home page

--- src/main/client/src/components/@commons/LinkButton.tsx
+++ src/main/client/src/components/@commons/LinkButton.tsx
@@ -5,16 +5,22 @@ import { useNavigate } from 'react-router-dom'
 export type LinkProps = {
   external?: boolean
   href: string
+  newTab?: boolean
 }
 
-export const LinkButton: React.FC<LinkProps & ButtonProps> = ({ external, href, children, ...props }) => {
+export const LinkButton: React.FC<LinkProps & ButtonProps> = ({ external, href, children, newTab = true, ...props }) => {
   const navigate = useNavigate()
   return (
     <Button
       {...props}
       onClick={() => {
-        if (external) window.open(href)
-        else navigate(href)
+        if (external) {
+          if (newTab) {
+            window.open(href)
+          } else {
+            location.href = href
+          }
+        } else navigate(href)
       }}
     >
       {children}
