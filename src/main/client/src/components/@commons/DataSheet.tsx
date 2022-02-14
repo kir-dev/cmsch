ec67cfa7f2e9c5479b7096ff4670966efd5e42dd
Bálint Berente <balint.berente@gmail.com>
2022-02-14 10:21:11 +0100

    Minor changes

--- src/main/client/src/components/@commons/DataSheet.tsx
+++ src/main/client/src/components/@commons/DataSheet.tsx
@@ -16,66 +16,71 @@ type DataSheetProps = {
  * @param dataObject Organization type object - resort or community
  * @constructor
  */
-export const DataSheet: React.FC<DataSheetProps> = ({ organization }) => (
-  <>
-    {!organization.hideName && <Heading textAlign={{ base: 'center', sm: 'left' }}>{organization.name}</Heading>}
-    <Flex flexDir={{ base: 'column-reverse', sm: 'row' }} justify="space-between" align="center" mt={{ base: 2, sm: 5 }}>
-      <VStack alignItems="flex-start" alignSelf={{ base: 'flex-start', sm: 'center' }}>
-        {organization.established && (
-          <DataField icon={<FaBusinessTime />} label="Alapítva">
-            <Box>{organization.established}</Box>
-          </DataField>
+export const DataSheet: React.FC<DataSheetProps> = ({ organization }) => {
+  const isDataAvailable = organization.established || organization.email || organization.members || organization.interests
+  return (
+    <>
+      {!organization.hideName && <Heading textAlign={{ base: 'center', sm: 'left' }}>{organization.name}</Heading>}
+      <Flex flexDir={{ base: 'column-reverse', sm: 'row' }} justify="space-between" align="center" mt={{ base: 2, sm: 5 }}>
+        {isDataAvailable && (
+          <VStack alignItems="flex-start" alignSelf={{ base: 'flex-start', sm: 'center' }}>
+            {organization.established && (
+              <DataField icon={<FaBusinessTime />} label="Alapítva">
+                <Box>{organization.established}</Box>
+              </DataField>
+            )}
+            {organization.email && (
+              <DataField icon={<FaAt />} label="E-mail">
+                <Box>{organization.email.replace('@', '[at]')}</Box>
+              </DataField>
+            )}
+            {organization.members && (
+              <DataField icon={<FaUsers />} label="Létszám">
+                <Box>{organization.members} fő</Box>
+              </DataField>
+            )}
+            {organization.interests && (
+              <Flex flexWrap="wrap">
+                {organization.interests.map((interest) => (
+                  <Box p={0.5}>
+                    <Tag colorScheme={organization.color} variant="solid" key={interest}>
+                      {interest}
+                    </Tag>
+                  </Box>
+                ))}
+              </Flex>
+            )}
+          </VStack>
         )}
-        {organization.email && (
-          <DataField icon={<FaAt />} label="E-mail">
-            <Box>{organization.email.replace('@', '[at]')}</Box>
-          </DataField>
-        )}
-        {organization.members && (
-          <DataField icon={<FaUsers />} label="Létszám">
-            <Box>{organization.members} fő</Box>
-          </DataField>
-        )}
-        {organization.interests && (
-          <Flex flexWrap="wrap">
-            {organization.interests.map((interest) => (
-              <Box p={0.5}>
-                <Tag colorScheme={organization.color} variant="solid" key={interest}>
-                  {interest}
-                </Tag>
-              </Box>
-            ))}
-          </Flex>
-        )}
-      </VStack>
-      {generateLogo(organization)}
-    </Flex>
-    {organization.descriptionParagraphs && generateParagraphs(organization.descriptionParagraphs)}
+        {generateLogo(organization)}
+      </Flex>
+      {organization.descriptionParagraphs && generateParagraphs(organization.descriptionParagraphs)}
 
-    <Wrap marginTop={10} justify={{ base: 'center', md: 'flex-start' }}>
-      {organization.website && (
-        <LinkButton href={organization.website} external leftIcon={<LinkIcon />} colorScheme={organization.color}>
-          Weboldal
-        </LinkButton>
-      )}
-      {organization.application && (
-        <LinkButton href={organization.application} external leftIcon={<EditIcon />} colorScheme="brand">
-          Jelentkezés
-        </LinkButton>
-      )}
-      {organization.facebook && (
-        <LinkButton href={organization.facebook} external leftIcon={<FaFacebook />} colorScheme="facebook">
-          Facebook
-        </LinkButton>
-      )}
-      {organization.instagram && (
-        <LinkButton href={organization.instagram} external leftIcon={<FaInstagram />} colorScheme="purple">
-          Instagram
-        </LinkButton>
-      )}
-    </Wrap>
-  </>
-)
+      <Wrap marginTop={10} justify={{ base: 'center', md: 'flex-start' }}>
+        {organization.website && (
+          <LinkButton href={organization.website} external leftIcon={<LinkIcon />} colorScheme={organization.color}>
+            Weboldal
+          </LinkButton>
+        )}
+        {organization.application && (
+          <LinkButton href={organization.application} external leftIcon={<EditIcon />} colorScheme="brand">
+            Jelentkezés
+          </LinkButton>
+        )}
+        {organization.facebook && (
+          <LinkButton href={organization.facebook} external leftIcon={<FaFacebook />} colorScheme="facebook">
+            Facebook
+          </LinkButton>
+        )}
+        {organization.instagram && (
+          <LinkButton href={organization.instagram} external leftIcon={<FaInstagram />} colorScheme="purple">
+            Instagram
+          </LinkButton>
+        )}
+      </Wrap>
+    </>
+  )
+}
 
 type DataFieldProps = {
   icon: JSX.Element
