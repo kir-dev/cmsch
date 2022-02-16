0f0545560a1a02b0a95e5d0a2c8e70b816486dd8
Gerviba <gerviba@sch.bme.hu>
2022-02-16 22:02:06 +0100

    Release 2.6.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ file("${projectDir}/src/main/resources/config/application.properties")
         applicationProperties.load(it)
     }
 group = "hu.bme.sch"
-version = "2.6.1"
+version = "2.6.2"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks {
--- src/main/kotlin/hu/bme/sch/cmsch/controller/api/MainController.kt
+++ src/main/kotlin/hu/bme/sch/cmsch/controller/api/MainController.kt
@@ -42,7 +42,7 @@ class MainController(
 
     @ResponseBody
     @GetMapping("/version")
-    fun version(): String = "2.6.1"
+    fun version(): String = "2.6.2"
 
     @ResponseBody
     @GetMapping("/time")
