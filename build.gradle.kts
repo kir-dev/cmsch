c023236f09bda6c6a30847613ba211d74fbe8080
Gergely Szabó <gerviba@sch.bme.hu>
2022-02-20 01:09:14 +0100

    Release 2.6.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,7 +17,7 @@ file("${projectDir}/src/main/resources/config/application.properties")
         applicationProperties.load(it)
     }
 group = "hu.bme.sch"
-version = "2.6.2"
+version = "2.6.3"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks {
