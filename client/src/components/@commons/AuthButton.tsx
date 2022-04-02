8de9825da1c29ce869598cf1e0383ebdcdd10546
triszt4n <trisztanpiller@gmail.com>
2022-04-02 16:33:17 +0200

    Logic prepared on frontend

--- client/src/components/@commons/AuthButton.tsx
+++ client/src/components/@commons/AuthButton.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Button } from '@chakra-ui/react'
 import { useAuthContext } from '../../utils/useAuthContext'
+import { API_BASE_URL } from 'utils/configurations'
 
 export const AuthButton: React.FC = () => {
-  const { isLoggedIn, login } = useAuthContext()
+  const { isLoggedIn } = useAuthContext()
+  const onLoginClicked = () => {
+    window.location.replace(`${API_BASE_URL}/control/login`)
+  }
+
   if (isLoggedIn) return null
   return (
-    <Button colorScheme="brand" onClick={login}>
+    <Button colorScheme="brand" onClick={onLoginClicked}>
       Bejelentkezés
     </Button>
   )
