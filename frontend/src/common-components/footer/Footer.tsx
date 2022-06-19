fe67961416f212233e86dd37f3b43ee82d42fcfd
Bálint Berente <balint.berente@gmail.com>
2022-06-19 22:35:20 +0200

    Footer and nav modifications

--- frontend/src/common-components/footer/Footer.tsx
+++ frontend/src/common-components/footer/Footer.tsx
@@ -3,32 +3,45 @@ import { FaHeart } from 'react-icons/fa'
 import { CmschContainer } from '../layout/CmschContainer'
 import { BUGREPORT_URL, KIRDEV_URL } from '../../util/configs/environment.config'
 import { customTheme } from '../../util/configs/theme.config'
+import Markdown from '../Markdown'
+import { useConfigContext } from '../../api/contexts/config/ConfigContext'
 
-export const Footer = () => (
-  <Box borderTopWidth={1} borderStyle="solid" borderColor={useColorModeValue('gray.200', 'gray.700')}>
-    <CmschContainer>
-      <Wrap justify="space-between" spacing={2} align="center">
-        <Flex direction="row" align="center">
-          <Text mr={2}>Made with</Text>
-          <FaHeart color="red" size="1.5rem" />
-          <Text ml={2}>by</Text>
-        </Flex>
-        <Image src={useColorModeValue('/img/kirdev.svg', '/img/kirdev-white.svg')} w="10rem" h="10rem" my={3} />
-        <HStack align="center">
-          <Link isExternal fontSize="xl" _hover={{ color: customTheme.colors.brand, textDecorationLine: 'underline' }} href={KIRDEV_URL}>
-            Weboldal
-          </Link>
-          <Text>|</Text>
-          <Link
-            isExternal
-            fontSize="xl"
-            _hover={{ color: customTheme.colors.kirDev, textDecorationLine: 'underline' }}
-            href={BUGREPORT_URL}
-          >
-            Kapcsolat
-          </Link>
-        </HStack>
-      </Wrap>
-    </CmschContainer>
-  </Box>
-)
+export const Footer = () => {
+  const config = useConfigContext()
+  return (
+    <Box borderTopWidth={1} borderStyle="solid" borderColor={useColorModeValue('gray.200', 'gray.700')}>
+      <CmschContainer>
+        <Wrap justify="space-between" spacing={2} align="center">
+          <Markdown text={config?.components.app.footerMessage} />
+          <Flex align="center" flexWrap="wrap">
+            <Flex direction="row" align="center">
+              <Text mr={2}>Made with</Text>
+              <FaHeart color="red" size="1.5rem" />
+              <Text ml={2}>by</Text>
+            </Flex>
+            <Image src={useColorModeValue('/img/kirdev.svg', '/img/kirdev-white.svg')} w="10rem" h="10rem" my={3} />
+            <HStack align="center">
+              <Link
+                isExternal
+                fontSize="xl"
+                _hover={{ color: customTheme.colors.brand, textDecorationLine: 'underline' }}
+                href={KIRDEV_URL}
+              >
+                Weboldal
+              </Link>
+              <Text>|</Text>
+              <Link
+                isExternal
+                fontSize="xl"
+                _hover={{ color: customTheme.colors.kirDev, textDecorationLine: 'underline' }}
+                href={BUGREPORT_URL}
+              >
+                Kapcsolat
+              </Link>
+            </HStack>
+          </Flex>
+        </Wrap>
+      </CmschContainer>
+    </Box>
+  )
+}
