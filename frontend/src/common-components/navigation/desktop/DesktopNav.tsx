8e0d089ab8bbc4ee6d9a9d430d321fd27efdeb59
triszt4n <trisztanpiller@gmail.com>
2022-06-19 16:45:14 +0200

    Better navbar code splitting

---
+++ frontend/src/common-components/navigation/desktop/DesktopNav.tsx
@@ -0,0 +1,22 @@
+import { Stack } from '@chakra-ui/react'
+import { NavItemWithChildren } from './NavItemWithChildren'
+import { NavItemNoChildren } from './NavItemNoChildren'
+import { useConfigContext } from '../../../api/contexts/config/useConfigContext'
+
+export const DesktopNav = () => {
+  const config = useConfigContext()
+
+  if (!config?.menu) return null
+
+  return (
+    <Stack direction="row" spacing={4}>
+      {config.menu.map((menu) =>
+        menu.children && menu.children.length > 0 ? (
+          <NavItemWithChildren key={menu.name} menu={menu} />
+        ) : (
+          <NavItemNoChildren key={menu.name} menu={menu} />
+        )
+      )}
+    </Stack>
+  )
+}
