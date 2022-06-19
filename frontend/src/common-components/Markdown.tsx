d173b38fab8dbe98f2429e08d490fcb3008f40d0
triszt4n <trisztanpiller@gmail.com>
2022-06-20 01:30:08 +0200

    Get rid of many console error messages

--- frontend/src/common-components/Markdown.tsx
+++ frontend/src/common-components/Markdown.tsx
@@ -1,12 +1,7 @@
 import ReactMarkdown from 'react-markdown'
-import { Paragraph } from './Paragraph'
 
 const Markdown = ({ text }: { text: string | undefined }) => {
-  return (
-    <Paragraph>
-      <ReactMarkdown children={text || ''} />
-    </Paragraph>
-  )
+  return <ReactMarkdown children={text || ''} />
 }
 
 export default Markdown
