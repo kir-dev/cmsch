7eaab9edbc663e8f2c0c6d0b3f75caf11c9c4d04
Bálint Berente <balint.berente@gmail.com>
2022-06-19 17:11:39 +0200

    Adde useQuery to Config fetch

--- frontend/src/api/hooks/commons/useProfileQuery.ts
+++ frontend/src/api/hooks/commons/useProfileQuery.ts
@@ -3,12 +3,12 @@ import { useQuery } from 'react-query'
 import { ProfileView } from '../../../util/views/profile.view'
 
 export const useProfileQuery = (isLoggedIn: boolean, onLoginFailure: (err: any) => void) => {
-  return useQuery(
+  return useQuery<ProfileView, Error, ProfileView>(
     'currentUser',
     async () => {
       const response = await axios.get<ProfileView>(`/api/profile`)
       return response.data
     },
-    { enabled: !!isLoggedIn, onError: onLoginFailure }
+    { enabled: isLoggedIn, onError: onLoginFailure }
   )
 }
