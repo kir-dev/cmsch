b6e8ead7738d80fd6795e667f1faa22095a73f1f
triszt4n <trisztanpiller@gmail.com>
2022-06-20 01:10:02 +0200

    Error pages and unknown navigation handling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,10 @@
 import { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import { CmschLayout } from './common-components/layout/CmschLayout'
+import { ErrorPage } from './pages/error/error.page'
 import IndexPage from './pages/index/index.page'
 import LoginPage from './pages/login/login.page'
 import { EnabledModules, GetRoutesForModules } from './util/configs/modules.config'
-import { ErrorPage } from './pages/error/error.page'
 
 export function App() {
   return (
@@ -16,7 +16,9 @@ export function App() {
             <Route index element={<IndexPage />} />
             <Route path="login" element={<LoginPage />} />
             <Route path="logout" element={<IndexPage />} />
+            {/** Error handling pages */}
             <Route path="error" element={<ErrorPage />} />
+            <Route path="*" element={<ErrorPage message="Hoppá, úgy tűnik egy olyan oldalt találtál, amely nem létezik többé!" />} />
           </Route>
         </Routes>
       </Suspense>
