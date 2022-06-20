92d12ce65d8351b6e2f13e4f0a55266136960c22
triszt4n <trisztanpiller@gmail.com>
2022-06-20 18:52:12 +0200

    backend bugfix: Let any user download the application props

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/app/ApplicationComponent.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/app/ApplicationComponent.kt
@@ -53,7 +53,7 @@ class ApplicationComponent(
     final override val menuDisplayName = null
 
     final override val minRole = MinRoleSettingProxy(componentSettingService, component,
-        "minRole", "", minRoleToEdit = RoleType.NOBODY,
+        "minRole", MinRoleSettingProxy.ALL_ROLES, minRoleToEdit = RoleType.NOBODY,
         fieldName = "Jogosultságok", description = "Melyik roleokkal nyitható meg az oldal"
     )
 
