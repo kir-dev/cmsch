35b320c91bd1bf01089068cf6823a47c03bb23cc
triszt4n <trisztanpiller@gmail.com>
2022-06-20 23:41:51 +0200

    Small fixes

--- frontend/src/pages/error/unauthorized.page.tsx
+++ frontend/src/pages/error/unauthorized.page.tsx
@@ -1,21 +1,27 @@
-import { ButtonGroup, Heading, Text } from '@chakra-ui/react'
+import { Button, ButtonGroup, Heading, Text } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet'
-import { AuthButton } from '../../common-components/AuthButton'
-import { LinkButton } from '../../common-components/LinkButton'
+import { useNavigate } from 'react-router-dom'
 import { CmschPage } from '../../common-components/layout/CmschPage'
+import { LinkButton } from '../../common-components/LinkButton'
 
-export const UnauthorizedPage = () => (
-  <CmschPage>
-    <Helmet title="Error 403" />
-    <Heading textAlign="center">Bejelentkezés szükséges</Heading>
-    <Text textAlign="center" color="gray.500" marginTop={10}>
-      Az oldal eléréséhez be kell jelentkezned!
-    </Text>
-    <ButtonGroup justifyContent="center" marginTop={10}>
-      <AuthButton />
-      <LinkButton href="/" variant="ghost">
-        Főoldal
-      </LinkButton>
-    </ButtonGroup>
-  </CmschPage>
-)
+export const UnauthorizedPage = () => {
+  const navigate = useNavigate()
+
+  return (
+    <CmschPage>
+      <Helmet title="Error 403" />
+      <Heading textAlign="center">Bejelentkezés szükséges</Heading>
+      <Text textAlign="center" color="gray.500" marginTop={10}>
+        Az oldal eléréséhez be kell jelentkezned!
+      </Text>
+      <ButtonGroup justifyContent="center" marginTop={10}>
+        <Button colorScheme="brand" onClick={() => navigate('/login')}>
+          Belépés
+        </Button>
+        <LinkButton href="/" variant="ghost">
+          Főoldal
+        </LinkButton>
+      </ButtonGroup>
+    </CmschPage>
+  )
+}
--- frontend/src/pages/profile/util/percentFunctions.ts
+++ frontend/src/pages/profile/util/percentFunctions.ts
@@ -1,11 +1,11 @@
 import { ProfileView } from '../../../util/views/profile.view'
 
 export const submittedPercent = (profile: ProfileView) => {
-  let res = (profile.submittedAchievementCount / profile.totalAchievementCount) * 100
+  let res = (profile.submittedTaskCount / profile.totalTaskCount) * 100
   return isNaN(res) ? 0 : res
 }
 
 export const completedPercent = (profile: ProfileView) => {
-  let res = (profile.completedAchievementCount / profile.totalAchievementCount) * 100
+  let res = (profile.completedTaskCount / profile.totalTaskCount) * 100
   return isNaN(res) ? 0 : res
 }
