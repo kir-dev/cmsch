a09fc88f0176375f29116ad701b487ad943de3e4
Sámuel Fekete <feketesamu@gmail.com>
2022-06-23 11:32:39 +0200

    deleted client folder

--- client/src/components/@commons/Paragraph.tsx
+++
@@ -1,8 +0,0 @@
-import { Text, TextProps } from '@chakra-ui/react'
-import { FC } from 'react'
-
-export const Paragraph: FC<TextProps> = ({ children, textAlign, ...props }) => (
-  <Text marginTop={5} {...props} textAlign={textAlign}>
-    {children}
-  </Text>
-)
--- client/src/types/MapImage.ts
+++
@@ -1,1 +0,0 @@
-export type MapImage = { dark: string; light: string }
