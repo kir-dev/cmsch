f7d6ba8e9be7a821525ae86a3b27e2e75fd796e3
DannyS123 <szdani2012@gmail.com>
2022-06-24 18:31:34 +0200

    newspages better design

--- frontend/src/pages/news/newsList.page.tsx
+++ frontend/src/pages/news/newsList.page.tsx
@@ -1,38 +1,41 @@
-import { Grid, Heading, Text, Box } from '@chakra-ui/react'
+import { Grid, Heading, Text, Box, GridItem, Divider } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet'
 import { useServiceContext } from '../../api/contexts/service/ServiceContext'
 import { useNewsListQuery } from '../../api/hooks/useNewsListQuery'
 import { CmschPage } from '../../common-components/layout/CmschPage'
-import NewsList from './components/newsListItem'
 import { Link, Navigate } from 'react-router-dom'
-import Markdown from '../../common-components/Markdown'
+import { stringifyTimeStamp } from '../../util/core-functions.util'
+import { NewsPreviewDTO } from '../../util/views/news.view'
 
 const NewsListPage = () => {
-  const newsList = useNewsListQuery(() => console.log('kill me'))
+  const newsList = useNewsListQuery(() => console.log('News list query failed!'))
   const { sendMessage } = useServiceContext()
 
-  if (typeof newsList.data === undefined) {
-    sendMessage('Hírek betöltése sikertelen! ')
+  if (newsList.isSuccess) {
+    return (
+      <CmschPage>
+        <Helmet title="Hírek" />
+        <Heading>Hírek</Heading>
+        <Text>{newsList.data.warningMessage}</Text>
+        <Grid templateColumns={{ base: 'repeat(1, auto)', md: 'repeat(2, auto)' }} gap={10} marginTop={10}>
+          {newsList.data.news.map((n: NewsPreviewDTO, i) => (
+            <GridItem key={i}>
+              <Link to={'/hirek/' + i}>
+                <Heading size="md" mt={'3rem'} mb={'0.5rem'} as={n.highlighted ? 'mark' : 'text'}>
+                  {n.title}
+                </Heading>
+                <Text>{stringifyTimeStamp(n.timestamp)}</Text>//TODO brief content?
+              </Link>
+              <Divider />
+            </GridItem>
+          ))}
+        </Grid>
+      </CmschPage>
+    )
+  } else {
+    sendMessage('Hírek betöltése sikertelen!')
     return <Navigate replace to="/error" />
   }
-
-  //<NewsList newsList={newsList.data.news} />
-
-  return (
-    <CmschPage>
-      <Helmet title="Hírek" />
-      <Heading>News list</Heading>
-      <Text>{newsList.data?.warningMessage}</Text>
-      {newsList.data?.news.map((n, i) => (
-        <Box key={i}>
-          <Box mt={'3rem'} mb={'0.5rem'} as={n.highlighted ? 'mark' : 'text'}>
-            <Link to={'/hirek/' + i}>{n.title}</Link>
-          </Box>
-          <Markdown text={n.timestamp.toLocaleString()} />
-        </Box>
-      ))}
-    </CmschPage>
-  )
 }
 
 export default NewsListPage
