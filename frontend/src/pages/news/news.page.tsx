f14d0173d6a90e583915a6c150414970899733e3
DannyS123 <szdani2012@gmail.com>
2022-06-24 18:31:34 +0200

    newspages better design

--- frontend/src/pages/news/news.page.tsx
+++ frontend/src/pages/news/news.page.tsx
@@ -1,26 +1,47 @@
-import { Heading } from '@chakra-ui/react'
+import { Button, Heading, Image, Text } from '@chakra-ui/react'
 import { toInteger } from 'lodash'
 import { Helmet } from 'react-helmet'
-import { useParams } from 'react-router-dom'
+import { Link, Navigate, useParams } from 'react-router-dom'
+import { useServiceContext } from '../../api/contexts/service/ServiceContext'
 import { useNewsListQuery } from '../../api/hooks/useNewsListQuery'
 import { CmschPage } from '../../common-components/layout/CmschPage'
 import Markdown from '../../common-components/Markdown'
+import { stringifyTimeStamp } from '../../util/core-functions.util'
 
 const NewsPage = () => {
-  const news = useNewsListQuery(() => console.log('kill me'))
-  const params = useParams()
+  const news = useNewsListQuery(() => console.log('News list query failed!'))
+  const { sendMessage } = useServiceContext()
 
-  const id = params.id ? params.id : 0
+  if (news.isSuccess) {
+    const params = useParams()
+    const id = toInteger(params.id)
+    const currentNews = news.data.news[id]
 
-  const currentNews = news.data?.news[toInteger(id)]
-
-  return (
-    <CmschPage>
-      <Helmet title="Hír" />
-      <Heading mb="1rem">{currentNews?.title}</Heading>
-      <Markdown text={currentNews?.content} />
-    </CmschPage>
-  )
+    return (
+      <CmschPage>
+        <Helmet title={currentNews.title} />
+        <Heading>{currentNews.title}</Heading>
+        <Text mb="1rem">{stringifyTimeStamp(currentNews.timestamp)}</Text>
+        <Markdown text={currentNews.content} />
+        <Image
+          mt="2rem"
+          mb="2rem"
+          display="block"
+          ml="auto"
+          mr="auto"
+          src={currentNews.imageUrl == '' ? 'https://picsum.photos/200' : currentNews.imageUrl} //TODO random képet kivenni
+          placeholder="ide kéne kép"
+          w="20rem"
+        />
+        <Link to="/hirek">
+          <Button>Vissza a hírekhez</Button>
+        </Link>
+      </CmschPage>
+    )
+  } else {
+    sendMessage('Hír betöltése sikertelen!')
+    return <Navigate replace to="/error" />
+  }
 }
 
 export default NewsPage
