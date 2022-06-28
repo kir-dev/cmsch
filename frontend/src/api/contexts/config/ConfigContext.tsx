d9b4a52eafe52117916a83e51f29ce43557b7dde
Sámuel Fekete <feketesamu@gmail.com>
2022-06-28 20:02:56 +0200

    first attempt

--- frontend/src/api/contexts/config/ConfigContext.tsx
+++ frontend/src/api/contexts/config/ConfigContext.tsx
@@ -2,13 +2,13 @@ import { createContext, useContext } from 'react'
 import { HasChildren } from '../../../util/react-types.util'
 import { ConfigDto } from './types'
 import { useConfigQuery } from '../../hooks/useConfigQuery'
+import { Loading } from '../../../common-components/Loading'
 
 export const ConfigContext = createContext<ConfigDto | undefined>(undefined)
 
 export const ConfigProvider = ({ children }: HasChildren) => {
   const { data, isLoading } = useConfigQuery((err) => console.error('[ERROR] at ConfigProvider', JSON.stringify(err, null, 2)))
-  // TODO: add loading screen
-  if (isLoading) return null
+  if (isLoading) return <Loading />
   return <ConfigContext.Provider value={data}>{children}</ConfigContext.Provider>
 }
 
