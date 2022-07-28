466f6157fe8c5b27461afdefa57c58534269009c
triszt4n <trisztanpiller@gmail.com>
2022-07-28 09:43:54 +0200

    Extra glow up

--- frontend/src/pages/news/components/NewsList.tsx
+++ frontend/src/pages/news/components/NewsList.tsx
@@ -1,4 +1,5 @@
 import { Grid, Heading, useBreakpointValue } from '@chakra-ui/react'
+import { useConfigContext } from '../../../api/contexts/config/ConfigContext'
 import { NewsArticleView } from '../../../util/views/news.view'
 import NewsListItem from './NewsListItem'
 
@@ -8,9 +9,10 @@ interface NewsListProps {
 
 const NewsList = ({ newsList }: NewsListProps) => {
   const highlighted = newsList.filter((news) => news.highlighted).sort((a, b) => b.timestamp - a.timestamp) // desc
+  const config = useConfigContext()
   return (
     <>
-      <Heading>Hírek</Heading>
+      <Heading>{config?.components.news.title}</Heading>
       <Grid templateColumns="repeat(1, 1fr)" gap={4} marginTop={4}>
         {highlighted.map((n: NewsArticleView, i) => (
           <NewsListItem news={n} fontSize="2xl" index={i} key={n.title + n.timestamp} />
