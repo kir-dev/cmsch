7a37d1cb9bc437ad71a8b628cc627634e9c828e3
Bálint Berente <balint.berente@gmail.com>
2022-07-28 10:11:55 +0200

    News fix

--- frontend/src/pages/news/news.page.tsx
+++ frontend/src/pages/news/news.page.tsx
@@ -1,40 +1,35 @@
-import { toInteger } from 'lodash'
 import { Helmet } from 'react-helmet-async'
 import { Navigate, useParams } from 'react-router-dom'
 import { useServiceContext } from '../../api/contexts/service/ServiceContext'
-import { useNewsListQuery } from '../../api/hooks/useNewsListQuery'
 import { CmschPage } from '../../common-components/layout/CmschPage'
 import { Loading } from '../../common-components/Loading'
 import News from './components/News'
-import { sortNewsList } from './util/sortNewsList'
 import { AbsolutePaths } from '../../util/paths'
+import { useNewsQuery } from '../../api/hooks/useNewsQuery'
 
 const NewsPage = () => {
-  const newsList = useNewsListQuery(() => console.log('News list query failed!'))
+  const { id } = useParams()
+  const { data, isLoading, isError } = useNewsQuery(id || '', () => console.log('News list query failed!'))
   const { sendMessage } = useServiceContext()
-  const params = useParams()
 
-  if (newsList.isLoading) {
+  if (isLoading) {
     return <Loading />
   }
 
-  if (newsList.isError) {
+  if (isError) {
     sendMessage('Hír betöltése sikertelen!')
     return <Navigate replace to={AbsolutePaths.ERROR} />
   }
 
-  if (typeof newsList.data === 'undefined') {
+  if (typeof data === 'undefined') {
     sendMessage('Hír betöltése sikertelen!\n Keresse az oldal fejlesztőit.')
     return <Navigate replace to={AbsolutePaths.ERROR} />
   }
 
-  const id = toInteger(params.id)
-  const currentNews = sortNewsList(newsList.data)[id]
-
   return (
     <CmschPage>
-      <Helmet title={currentNews.title} />
-      <News news={currentNews} />
+      <Helmet title={data.title} />
+      <News news={data} />
     </CmschPage>
   )
 }
