5588b5a52c70e7bd7d9987c90dcd40f913034091
Bálint Berente <balint.berente@gmail.com>
2022-07-28 10:11:55 +0200

    News fix

--- frontend/src/api/hooks/useNewsQuery.ts
+++ frontend/src/api/hooks/useNewsQuery.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 import { NewsArticleView } from '../../util/views/news.view'
 
-export const useNewsQuery = (id: Number, onError: (err: any) => void) => {
+export const useNewsQuery = (id: string, onError: (err: any) => void) => {
   return useQuery<NewsArticleView, Error, NewsArticleView>(
     ['newsItem', id],
     async () => {
