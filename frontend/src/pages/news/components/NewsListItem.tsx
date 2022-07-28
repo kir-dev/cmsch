b70dd1459cff79ebbd8c4eff8e4fbb299af92e06
triszt4n <trisztanpiller@gmail.com>
2022-07-28 11:14:53 +0200

    Support news component showDetails

--- frontend/src/pages/news/components/NewsListItem.tsx
+++ frontend/src/pages/news/components/NewsListItem.tsx
@@ -1,38 +1,41 @@
-import { Divider, GridItem, Heading, Text, Link as ChakraLink, Box, HStack, Icon } from '@chakra-ui/react'
-import { Link } from 'react-router-dom'
-import { stringifyTimeStamp } from '../../../util/core-functions.util'
-import { NewsArticleView } from '../../../util/views/news.view'
-import { AbsolutePaths } from '../../../util/paths'
-import Markdown from '../../../common-components/Markdown'
-import { ExternalLinkIcon, StarIcon } from '@chakra-ui/icons'
-import { BsExclamationCircle } from 'react-icons/bs'
+import { Box, GridItem, Heading, HStack, Icon, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { FaStar } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import Markdown from '../../../common-components/Markdown'
+import { stringifyTimeStamp } from '../../../util/core-functions.util'
+import { AbsolutePaths } from '../../../util/paths'
+import { NewsArticleView } from '../../../util/views/news.view'
 
-interface NewsListItemProps {
+type Props = {
   news: NewsArticleView
   fontSize: string
-  index: Number
+  useLink?: boolean
 }
 
-const NewsListItem = ({ news, fontSize, index }: NewsListItemProps) => {
+const NewsListItem = ({ news, fontSize, useLink }: Props) => {
+  console.log('asd', useLink)
   return (
-    <GridItem borderRadius="base" borderColor="whiteAlpha.200" borderWidth="1px" p={4}>
-      <Link to={`${AbsolutePaths.NEWS}/${news.url}`}>
-        <HStack justifyContent="space-between">
-          <Box fontSize="sm" mb={2} fontWeight={300}>
-            Közzétéve: {stringifyTimeStamp(news.timestamp)}
+    <GridItem as={LinkBox} borderRadius="base" borderColor="whiteAlpha.200" borderWidth="1px" p={4}>
+      <HStack justifyContent="space-between">
+        <Box fontSize="sm" mb={2} fontWeight={300}>
+          Közzétéve: {stringifyTimeStamp(news.timestamp)}
+        </Box>
+        {news.highlighted && (
+          <Box>
+            <Icon as={FaStar} color="brand.100" w={8} h={8} />
           </Box>
-          {news.highlighted && (
-            <Box>
-              <Icon as={FaStar} color="brand.100" w={8} h={8} />
-            </Box>
-          )}
-        </HStack>
-        <Heading size={fontSize} my={2}>
-          {news.title}
-        </Heading>
-        {news.briefContent && <Markdown text={news.briefContent} />}
-      </Link>
+        )}
+      </HStack>
+      <Heading size={fontSize} my={2}>
+        {useLink ? (
+          <LinkOverlay as={Link} to={`${AbsolutePaths.NEWS}/${news.url}`}>
+            {news.title}
+          </LinkOverlay>
+        ) : (
+          news.title
+        )}
+      </Heading>
+      {news.briefContent && <Markdown text={news.briefContent} />}
     </GridItem>
   )
 }
