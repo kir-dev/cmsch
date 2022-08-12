9a721b5531789de294acde017fd05cd00d129945
Bálint Berente <balint.berente@gmail.com>
2022-08-13 00:12:03 +0200

    Added error boundary

---
+++ frontend/src/util/errorBoundary.tsx
@@ -0,0 +1,45 @@
+import React, { ErrorInfo } from 'react'
+import { HasChildren } from './react-types.util'
+import { Helmet } from 'react-helmet-async'
+import { ButtonGroup, Heading, Text } from '@chakra-ui/react'
+import { LinkButton } from '../common-components/LinkButton'
+import { CmschPage } from '../common-components/layout/CmschPage'
+
+interface State {
+  hasError: boolean
+}
+
+export class ErrorBoundary extends React.Component<HasChildren, State> {
+  public state: State = {
+    hasError: false
+  }
+
+  static getDerivedStateFromError(_: Error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error(errorInfo)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return (
+        <CmschPage>
+          <Helmet title="Hiba" />
+          <Heading textAlign="center">Hiba történt</Heading>
+          <Text textAlign="center" color="gray.500" marginTop={10}>
+            Sajnos ilyennel még nem találkoztunk. Légyszíves ezt jelezd a fejlesztőknek!
+          </Text>
+          <ButtonGroup justifyContent="center" marginTop={10}>
+            <LinkButton href="/" colorScheme="brand">
+              Főoldal
+            </LinkButton>
+          </ButtonGroup>
+        </CmschPage>
+      )
+    }
+
+    return this.props.children
+  }
+}
