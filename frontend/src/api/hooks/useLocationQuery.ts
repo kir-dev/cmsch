19b52bd600a47e5f2c6e27838e7d8a0fe62c8075
Sámuel Fekete <feketesamu@gmail.com>
2022-08-25 23:48:25 +0200

    dots actually move after new data

--- frontend/src/api/hooks/useLocationQuery.ts
+++ frontend/src/api/hooks/useLocationQuery.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 import { GroupMemberLocationView } from '../../util/views/groupMemberLocation.view'
 
-export const useLocationQuery = (onError: (err: any) => void) => {
+export const useLocationQuery = (onError: (err: any) => void, onSuccess: () => void) => {
   return useQuery<GroupMemberLocationView[], Error, GroupMemberLocationView[]>(
     'locations',
     async () => {
       const response = await axios.get<GroupMemberLocationView[]>(`/api/track-my-group`)
       return response.data
     },
-    { onError, refetchInterval: 30000 }
+    { onError, onSuccess, refetchInterval: 15000 }
   )
 }
