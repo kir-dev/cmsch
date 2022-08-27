d0b726afc37cf6da6c6a0d32f0493c88ffc657ef
Sámuel Fekete <feketesamu@gmail.com>
2022-08-27 20:30:17 +0200

    map fixes

--- frontend/src/pages/profile/components/openlayers/Layers/VectorLayer.tsx
+++ frontend/src/pages/profile/components/openlayers/Layers/VectorLayer.tsx
@@ -1,9 +1,8 @@
-import { useContext, useEffect, useState } from 'react'
+import { useContext, useEffect } from 'react'
 import MapContext from '../MapContext'
 import OLVectorLayer from 'ol/layer/Vector'
 import OLVectorSource from 'ol/source/Vector'
 import Style from 'ol/style/Style'
-import { Geometry } from 'ol/geom'
 
 type VectorLayerProps = {
   source: OLVectorSource
@@ -13,23 +12,18 @@ type VectorLayerProps = {
 
 const VectorLayer = ({ source, style, zIndex = 0 }: VectorLayerProps) => {
   const map = useContext(MapContext)
-  const [layer, setLayer] = useState<OLVectorLayer<OLVectorSource<Geometry>>>()
 
   useEffect(() => {
     if (!map) return
-    if (layer) {
-      map.removeLayer(layer)
-    }
-    let vectorLayer = new OLVectorLayer({
+    const vectorLayer = new OLVectorLayer({
       source,
-      style
+      style,
+      zIndex
     })
-    setLayer(vectorLayer)
     map.addLayer(vectorLayer)
-    vectorLayer.setZIndex(zIndex)
     return () => {
-      if (map && layer) {
-        map.removeLayer(layer)
+      if (map) {
+        map.removeLayer(vectorLayer)
       }
     }
   }, [map, source])
