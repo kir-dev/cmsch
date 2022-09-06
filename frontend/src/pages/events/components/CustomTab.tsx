9758e2cf6b6dfd57b38cc27de9e4a67e1f8fa981
Sámuel Fekete <feketesamu@gmail.com>
2022-09-06 22:44:42 +0200

    event and task ui changes

--- frontend/src/pages/events/components/CustomTab.tsx
+++ frontend/src/pages/events/components/CustomTab.tsx
@@ -5,12 +5,12 @@ export const CustomTab = ({ children }: HasChildren) => {
   const styles = {
     color: useColorModeValue('brand.500', 'brand.200'),
     borderRadius: '5px 5px 0 0',
-    borderBottomColor: useColorModeValue('brand.500', 'brand.200'),
+    borderBottomColor: useColorModeValue('brand.500', 'brand.500'),
     borderBottomWidth: '2px'
   }
   const selectedStyles = {
-    color: useColorModeValue('whiteAlpha.200', 'gray.800'),
-    bg: useColorModeValue('brand.500', 'brand.200')
+    color: 'white',
+    bg: useColorModeValue('brand.500', 'brand.500')
   }
   return (
     <Tab _selected={selectedStyles} style={styles}>
