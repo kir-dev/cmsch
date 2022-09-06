4bbcac8e77b5c1b6db8dbcf1901b12336dba2bff
Sámuel Fekete <feketesamu@gmail.com>
2022-09-06 22:20:26 +0200

    event refactor

--- frontend/src/pages/events/components/EventList.tsx
+++ frontend/src/pages/events/components/EventList.tsx
@@ -1,4 +1,4 @@
-import { Grid, useBreakpointValue } from '@chakra-ui/react'
+import { Grid, useBreakpointValue, VStack } from '@chakra-ui/react'
 import { useConfigContext } from '../../../api/contexts/config/ConfigContext'
 import { EventListView } from '../../../util/views/event.view'
 import EventListItem from './EventListItem'
@@ -10,11 +10,11 @@ interface EventListProps {
 const EventList = ({ eventList }: EventListProps) => {
   const config = useConfigContext()
   return (
-    <Grid templateColumns={`repeat(${useBreakpointValue({ base: 1, md: 2 })}, 1fr)`} gap={3}>
+    <VStack spacing={3}>
       {eventList.map((e: EventListView) => (
         <EventListItem event={e} key={e.url + e.timestampStart} useLink={config?.components.event.enableDetailedView} />
       ))}
-    </Grid>
+    </VStack>
   )
 }
 
--- frontend/src/util/core-functions.util.ts
+++ frontend/src/util/core-functions.util.ts
@@ -27,11 +27,38 @@ export const TIMESTAMP_OPTIONS: Intl.DateTimeFormatOptions = {
   minute: '2-digit'
 }
 
+export const DETAILED_TIMESTAMP_OPTIONS: Intl.DateTimeFormatOptions = {
+  month: 'short',
+  day: '2-digit',
+  hour: '2-digit',
+  minute: '2-digit',
+  weekday: 'long'
+}
+
 export const GROUP_BY_DAY_OPTIONS: Intl.DateTimeFormatOptions = {
   month: 'short',
-  day: '2-digit'
+  day: '2-digit',
+  weekday: 'long'
+}
+
+export const HOUR_AND_MIN_OPTIONS: Intl.DateTimeFormatOptions = {
+  hour: '2-digit',
+  minute: '2-digit'
 }
 
 export const stringifyTimeStamp = (timeStamp: number, options: Intl.DateTimeFormatOptions = TIMESTAMP_OPTIONS): string => {
   return new Date(timeStamp * 1000).toLocaleString('hu-HU', options)
+}
+
+export const stringifyTimeRange = (fromTimeStamp: number, toTimeStamp: number) => {
+  const from = new Date(fromTimeStamp * 1000)
+  const to = new Date(toTimeStamp * 1000)
+  if (from.getDate() === to.getDate() && from.getMonth() === from.getMonth()) {
+    return `${from.toLocaleString('hu-HU', GROUP_BY_DAY_OPTIONS)} ${from.toLocaleString(
+      'hu-HU',
+      HOUR_AND_MIN_OPTIONS
+    )} - ${to.toLocaleString('hu-HU', HOUR_AND_MIN_OPTIONS)}`
+  } else {
+    return `${from.toLocaleString('hu-HU', TIMESTAMP_OPTIONS)} - ${to.toLocaleString('hu-HU', TIMESTAMP_OPTIONS)}`
+  }
 }
