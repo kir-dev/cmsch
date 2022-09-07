3d9a656fe37aceae3188c3f9f6c2b70680acd366
Gerviba <gerviba@sch.bme.hu>
2022-09-07 18:24:46 +0200

    #454 Update Spring version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -1,14 +1,14 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "2.6.6"
+    id("org.springframework.boot") version "2.7.3"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     kotlin("jvm") version "1.6.10"
     kotlin("plugin.spring") version "1.6.10"
 }
 
 group = "hu.bme.sch"
-version = "3.0.610"
+version = "3.622.0"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 tasks {
@@ -29,7 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
-    implementation("com.itextpdf:itext7-core:7.2.1")
+    implementation("com.itextpdf:itext7-core:7.2.3")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
@@ -37,14 +37,14 @@ dependencies {
     implementation("mysql:mysql-connector-java")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.springframework.security:spring-security-test")
-    implementation("org.springdoc:springdoc-openapi-ui:1.6.9")
-    implementation("org.springdoc:springdoc-openapi-security:1.6.9")
-    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.9")
-    implementation("com.google.zxing:core:3.4.1")
-    implementation("com.google.zxing:javase:3.4.1")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.11")
+    implementation("org.springdoc:springdoc-openapi-security:1.6.11")
+    implementation("org.springdoc:springdoc-openapi-kotlin:1.6.11")
+    implementation("com.google.zxing:core:3.5.0")
+    implementation("com.google.zxing:javase:3.5.0")
     implementation("io.jsonwebtoken:jjwt:0.9.1")
     implementation("com.fasterxml.uuid:java-uuid-generator:3.2.0")
-    implementation("org.commonmark:commonmark:0.17.2")
+    implementation("org.commonmark:commonmark:0.19.0")
 }
 
 tasks.withType<KotlinCompile> {
