20dca57c002a4f6e7b63fad3864cfc8b6f42cfd5
Bálint Berente <balint.berente@gmail.com>
2022-09-12 23:52:36 +0200

    Frontend fixes #n+1

--- frontend/src/api/hooks/team/queries/useTeamDetails.ts
+++ frontend/src/api/hooks/team/queries/useTeamDetails.ts
@@ -4,7 +4,7 @@ import { TeamView } from '../../../../util/views/team.view'
 
 export const useTeamDetails = (id?: string, onError?: (err: any) => void) => {
   return useQuery<TeamView, Error>(
-    ['team', 'details'],
+    ['team', 'details', id],
     async () => {
       if (!id) throw new Error('Nincs ID!')
       const response = await axios.get<TeamView>(`/api/team/${id}`)
