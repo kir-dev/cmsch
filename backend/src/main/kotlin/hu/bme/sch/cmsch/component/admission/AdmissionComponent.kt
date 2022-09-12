e6ce2c82a9a51025d9c3177593cc989624af0d50
Gerviba <gerviba@sch.bme.hu>
2022-09-12 23:51:23 +0200

    Fix some team component issue

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/admission/AdmissionComponent.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/admission/AdmissionComponent.kt
@@ -46,7 +46,7 @@ class AdmissionComponent(
     final override val menuDisplayName = null
 
     final override val minRole = MinRoleSettingProxy(componentSettingService, component,
-        "minRole", MinRoleSettingProxy.ALL_ROLES, minRoleToEdit = RoleType.NOBODY,
+        "minRole", MinRoleSettingProxy.ALL_ROLES, minRoleToEdit = RoleType.SUPERUSER,
         fieldName = "Jogosultságok", description = "Melyik roleokkal nyitható meg az oldal"
     )
 
