b3da486bd1a71d08c17c2b8f11a4c617a0f1a878
Bálint Berente <balint.berente@gmail.com>
2022-09-13 21:19:27 +0200

    RelType based Team Admin Site

--- frontend/src/common-components/layout/CmschPage.tsx
+++ frontend/src/common-components/layout/CmschPage.tsx
@@ -1,16 +1,27 @@
 import { CmschContainer } from './CmschContainer'
-import { Outlet } from 'react-router-dom'
+import { Navigate, Outlet } from 'react-router-dom'
 import { HasChildren } from '../../util/react-types.util'
+import { useAuthContext } from '../../api/contexts/auth/useAuthContext'
+import { RoleType } from '../../util/views/profile.view'
+import { Loading } from '../Loading'
 //import { AbsolutePaths } from '../../util/paths'
 
 type Props = {
   loginRequired?: boolean
   groupRequired?: boolean
+  minRole?: RoleType
 } & HasChildren
 
-export const CmschPage = ({ loginRequired, groupRequired, children, ...props }: Props) => {
-  // const { isLoggedIn, profile } = useAuthContext()
+export const CmschPage = ({ loginRequired, groupRequired, children, minRole, ...props }: Props) => {
+  const { profile, profileLoading } = useAuthContext()
 
+  if (minRole && minRole > 0) {
+    if (profileLoading) {
+      return <Loading />
+    } else if (!profile?.role || RoleType[profile?.role] < minRole) {
+      return <Navigate to="/" replace />
+    }
+  }
   // if (loginRequired && !isLoggedIn) return <UnauthorizedPage />
 
   // if (groupRequired && profile?.groupSelectionAllowed) return <Navigate to={`${AbsolutePaths.PROFILE}tankor-modositas`} />
