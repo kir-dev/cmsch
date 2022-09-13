5fc40f0e74ac62fba8724c267b8312e5bdbaffdb
Sámuel Fekete <feketesamu@gmail.com>
2022-09-13 20:39:22 +0200

    riddle refactor, submission not tested

--- frontend/src/pages/riddle/riddle.page.tsx
+++ frontend/src/pages/riddle/riddle.page.tsx
@@ -24,93 +24,90 @@ import { Loading } from '../../common-components/Loading'
 import { CmschPage } from '../../common-components/layout/CmschPage'
 import { AbsolutePaths, Paths } from '../../util/paths'
 import { l } from '../../util/language'
+import { useRiddleDetailsQuery } from '../../api/hooks/useRiddleDeatilsQuery'
+import { useServiceContext } from '../../api/contexts/service/ServiceContext'
+import { useRiddleSubmitMutation } from '../../api/hooks/useRiddleSubmitMutation'
+import { API_BASE_URL } from '../../util/configs/environment.config'
+import { useRiddleHintQuery } from '../../api/hooks/useRiddleHintQuery'
 
 const RiddlePage = () => {
   const { id } = useParams()
   const navigate = useNavigate()
   const toast = useToast()
   const toastIdRef = useRef<ToastId | null>(null)
-
-  const [riddle, setRiddle] = useState<Riddle>({ id: 1, title: '', imageUrl: '', solved: false, hint: undefined })
-
-  const [loading, setLoading] = useState<boolean>(false)
+  const { sendMessage } = useServiceContext()
   const solutionInput = useRef<HTMLInputElement>(null)
+  let queryResult = useRiddleDetailsQuery(() => toast({ title: l('riddle-query-failed'), status: 'error' }), id || '')
+  let hintQuery = useRiddleHintQuery(() => toast({ title: l('riddle-query-failed'), status: 'error' }), id || '')
+  const submissionMutation = useRiddleSubmitMutation()
 
-  useEffect(() => {
-    setLoading(true)
-    axios
-      .get<Riddle>(`/api/${Paths.RIDDLE}/${id}`)
-      .then((res) => {
-        setRiddle(res.data)
-        setLoading(false)
-      })
-      .catch(() => {
-        console.error('Nem sikerült lekérni a Riddle-t.')
-      })
-  }, [setRiddle])
+  if (queryResult.isError) {
+    sendMessage(l('riddle-query-failed') + queryResult.error.message)
+    return <Navigate replace to={AbsolutePaths.ERROR} />
+  }
+
+  if (queryResult.isLoading) {
+    return <Loading />
+  }
 
   if (!id) return <Navigate to="/" replace />
 
   const submitSolution = (event: FormEvent) => {
     event.preventDefault()
     const solution = solutionInput?.current?.value
-    axios
-      .post<RiddleSubmissonResult>(`/api/${Paths.RIDDLE}/${id}`, { solution: solution })
-      .then((res) => {
-        if (res.data.status === RiddleSubmissonStatus.WRONG) {
-          if (toastIdRef.current) {
-            toast.close(toastIdRef.current)
+    submissionMutation.mutate(
+      { solution: solution || '', id: id || '' },
+      {
+        onSuccess: (result) => {
+          if (result.status === RiddleSubmissonStatus.WRONG) {
+            if (toastIdRef.current) {
+              toast.close(toastIdRef.current)
+            }
+            toastIdRef.current =
+              toast({
+                title: l('riddle-incorrect-title'),
+                description: l('riddle-incorrect-description'),
+                status: 'error',
+                duration: 9000,
+                isClosable: true
+              }) || null
           }
-          toastIdRef.current =
+          if (result.status === RiddleSubmissonStatus.CORRECT && result.nextId) {
+            navigate(`${AbsolutePaths.RIDDLE}/${result.nextId}`)
+            const input = document.getElementById('solution') as HTMLInputElement
+            input.value = ''
+            hintQuery = useRiddleHintQuery(
+              () => toast({ title: l('riddle-query-failed'), status: 'error' }),
+              result.nextId.toString() || ''
+            )
+            queryResult = useRiddleDetailsQuery(
+              () => toast({ title: l('riddle-query-failed'), status: 'error' }),
+              result.nextId.toString() || ''
+            )
             toast({
-              title: l('riddle-incorrect-title'),
-              description: l('riddle-incorrect-description'),
-              status: 'error',
+              title: l('riddle-correct-title'),
+              description: l('riddle-correct-description'),
+              status: 'success',
               duration: 9000,
               isClosable: true
-            }) || null
+            })
+          }
+          if (result.status === RiddleSubmissonStatus.CORRECT && !result.nextId) {
+            navigate(AbsolutePaths.RIDDLE)
+            toast({
+              title: l('riddle-completed-title'),
+              description: l('riddle-completed-description'),
+              status: 'success',
+              duration: 9000,
+              isClosable: true
+            })
+          }
+        },
+        onError: () => {
+          console.error('Nem sikerült beküldeni a Riddle-t.', { toast: true })
         }
-        if (res.data.status === RiddleSubmissonStatus.CORRECT && res.data.nextId) {
-          navigate(`${AbsolutePaths.RIDDLE}/${res.data.nextId}`)
-          const input = document.getElementById('solution') as HTMLInputElement
-          input.value = ''
-          axios.get<Riddle>(`/api/${Paths.RIDDLE}/${res.data.nextId}`).then((resp) => {
-            setRiddle(resp.data)
-          })
-          toast({
-            title: l('riddle-correct-title'),
-            description: l('riddle-correct-description'),
-            status: 'success',
-            duration: 9000,
-            isClosable: true
-          })
-        }
-        if (res.data.status === RiddleSubmissonStatus.CORRECT && !res.data.nextId) {
-          navigate(AbsolutePaths.RIDDLE)
-          toast({
-            title: l('riddle-completed-title'),
-            description: l('riddle-completed-description'),
-            status: 'success',
-            duration: 9000,
-            isClosable: true
-          })
-        }
-      })
-      .catch(() => {
-        console.error('Nem sikerült beküldeni a Riddle-t.', { toast: true })
-      })
-  }
-
-  const getHint = () => {
-    return axios
-      .put<Hint>(`/api/${Paths.RIDDLE}/${id}/hint`)
-      .then((res) => {
-        const newRiddle = { ...riddle, hint: res.data.hint }
-        setRiddle(newRiddle)
-      })
-      .catch(() => {
-        console.error('Nem sikerült lekérni a hint-et.', { toast: true })
-      })
+      }
+    )
   }
 
   const breadcrumbItems = [
@@ -119,22 +116,20 @@ const RiddlePage = () => {
       to: AbsolutePaths.RIDDLE
     },
     {
-      title: riddle.title
+      title: queryResult.data?.title
     }
   ]
 
-  if (loading) return <Loading />
-
   return (
     <CmschPage loginRequired groupRequired>
-      <Helmet title={riddle.title} />
+      <Helmet title={queryResult.data?.title} />
       <CustomBreadcrumb items={breadcrumbItems} />
 
-      <Flex align="center">
-        <Heading> {riddle.title} </Heading>
-      </Flex>
+      <Heading my={5}> {queryResult.data?.title} </Heading>
       <Box maxW="100%" w="30rem" mx="auto">
-        {riddle.imageUrl && <Image width="100%" src={`/cdn/${riddle.imageUrl}`} alt="Riddle Kép" borderRadius="md" />}
+        {queryResult.data?.imageUrl && (
+          <Image width="100%" src={`${API_BASE_URL}/cdn/${queryResult.data?.imageUrl}`} alt="Riddle Kép" borderRadius="md" />
+        )}
         <Box
           as="form"
           onSubmit={submitSolution}
@@ -146,20 +141,20 @@ const RiddlePage = () => {
         >
           <FormControl>
             <FormLabel htmlFor="solution">Megoldásom:</FormLabel>
-            <Input ref={solutionInput} id="solution" name="solution" autoComplete="off" readOnly={riddle.solved} />
+            <Input ref={solutionInput} id="solution" name="solution" autoComplete="off" readOnly={queryResult.data?.solved} />
           </FormControl>
 
           <VStack spacing={5} mt={10}>
             <Button type="submit" colorScheme="brand" width="100%">
               Beadom
             </Button>
-            {riddle.hint ? (
+            {hintQuery.isSuccess ? (
               <Alert status="info" borderRadius="md">
                 <AlertIcon />
-                {riddle.hint}
+                {hintQuery.data.hint}
               </Alert>
             ) : (
-              <Button onClick={getHint} width="100%" colorScheme="blue" variant="outline">
+              <Button onClick={() => hintQuery.refetch()} width="100%" colorScheme="blue" variant="outline">
                 Hintet kérek!
               </Button>
             )}
