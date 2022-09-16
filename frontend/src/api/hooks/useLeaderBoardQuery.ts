3bd8222f542b7fa6508aac4d3319225ab24245c5
Sámuel Fekete <feketesamu@gmail.com>
2022-09-16 12:53:22 +0200

    ugly solution

--- frontend/src/api/hooks/useLeaderBoardQuery.ts
+++ frontend/src/api/hooks/useLeaderBoardQuery.ts
@@ -2,11 +2,41 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 import { LeaderBoardView } from '../../util/views/leaderBoardView'
 
-export const useLeaderBoardQuery = (onError: (err: any) => void) => {
+type TempLeaderBoardItemView = {
+  id?: number
+  name: string
+  groupName: string
+  score?: number
+  items?: Object
+  total?: number
+}
+
+export type TempLeaderBoardView = {
+  userScore?: number
+  userBoard?: Array<TempLeaderBoardItemView>
+  groupScore?: number
+  groupBoard?: Array<TempLeaderBoardItemView>
+}
+
+export const useLeaderBoardQuery = (onError: (err: any) => void, detailed: boolean = false) => {
   async function fetchLeaderBoard() {
-    const result = await axios.get<LeaderBoardView>(`/api/leaderboard`)
-    return result.data
+    const result = await axios.get<TempLeaderBoardView>(`/api/${detailed ? 'detailed-' : ''}leaderboard`)
+    return {
+      ...result.data,
+      userBoard: result.data.userBoard
+        ? result.data.userBoard.map((boardItems) => ({
+            ...boardItems,
+            items: Object.entries(boardItems.items || {}).map(([key, value]) => ({ name: key, value }))
+          }))
+        : undefined,
+      groupBoard: result.data.groupBoard
+        ? result.data.groupBoard.map((boardItems) => ({
+            ...boardItems,
+            items: Object.entries(boardItems.items || {}).map(([key, value]) => ({ name: key, value }))
+          }))
+        : undefined
+    }
   }
 
-  return useQuery<LeaderBoardView, Error, LeaderBoardView>(['leaderboard'], fetchLeaderBoard, { onError: onError })
+  return useQuery<TempLeaderBoardView, Error, LeaderBoardView>(['leaderboard'], fetchLeaderBoard, { onError: onError })
 }
