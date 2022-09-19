a23ef06989805e893ee64fe0a9f3a05cc6aff97e
Gerviba <gerviba@sch.bme.hu>
2022-09-19 23:25:55 +0200

    Fix riddle component 2

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/riddle/RiddleEntity.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/riddle/RiddleEntity.kt
@@ -12,7 +12,7 @@ import javax.persistence.*
 
 @Entity
 @Table(name="riddles")
-@ConditionalOnBean(RiddleService::class)
+@ConditionalOnBean(RiddleComponent::class)
 data class RiddleEntity(
 
     @Id
