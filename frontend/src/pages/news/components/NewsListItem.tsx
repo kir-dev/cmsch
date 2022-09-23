599db7e0439f7968ecae423c917e4878cf835f81
Bálint Berente <balint.berente@gmail.com>
2022-09-23 23:18:39 +0200

    News highlight improved

--- frontend/src/pages/news/components/NewsListItem.tsx
+++ frontend/src/pages/news/components/NewsListItem.tsx
@@ -1,5 +1,5 @@
 import { Box, GridItem, Heading, HStack, Icon, LinkBox, LinkOverlay } from '@chakra-ui/react'
-import { FaStar } from 'react-icons/fa'
+import { FaExclamation } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Markdown from '../../../common-components/Markdown'
 import { stringifyTimeStamp } from '../../../util/core-functions.util'
@@ -14,14 +14,14 @@ type Props = {
 
 const NewsListItem = ({ news, fontSize, useLink }: Props) => {
   return (
-    <GridItem as={LinkBox} borderRadius="base" borderColor="whiteAlpha.200" borderWidth="1px" p={4}>
+    <GridItem as={LinkBox} borderRadius="base" borderColor={news.highlighted ? 'brand.200' : 'whiteAlpha.200'} borderWidth="1px" p={4}>
       <HStack justifyContent="space-between">
         <Box fontSize="sm" mb={2} fontWeight={300}>
           Közzétéve: {stringifyTimeStamp(news.timestamp)}
         </Box>
         {news.highlighted && (
           <Box>
-            <Icon as={FaStar} color="brand.100" w={8} h={8} />
+            <Icon as={FaExclamation} color="brand.200" w={8} h={8} />
           </Box>
         )}
       </HStack>
