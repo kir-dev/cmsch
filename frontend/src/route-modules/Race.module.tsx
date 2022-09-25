e5929ccb93b3fd0393cfb7d472c3b6432b9c73cc
DannyS123 <szdani2012@gmail.com>
2022-09-25 22:08:47 +0200

    custum sormeresek

--- frontend/src/route-modules/Race.module.tsx
+++ frontend/src/route-modules/Race.module.tsx
@@ -7,6 +7,7 @@ const RacePage = lazy(() => import('../pages/race/race.page'))
 export function RaceModule() {
   return (
     <Route path={Paths.RACE}>
+      <Route path=":category" element={<RacePage />} />
       <Route index element={<RacePage />} />
     </Route>
   )
--- frontend/src/util/views/race.view.ts
+++ frontend/src/util/views/race.view.ts
@@ -1,6 +1,8 @@
 import { LeaderBoardItemView } from './leaderBoardView'
 
 export type RaceView = {
+  categoryName: string
+  description: string
   place?: number
   bestTime?: number
   board: LeaderBoardItemView[]
