b8f0807675202cd9d0b81eed844160acd6eadd63
Gerviba <gerviba@sch.bme.hu>
2023-02-21 22:48:56 +0100

    Add communities

---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/communities/CommunitiesApiController.kt
@@ -0,0 +1,49 @@
+package hu.bme.sch.cmsch.component.communities
+
+import com.fasterxml.jackson.annotation.JsonView
+import hu.bme.sch.cmsch.dto.FullDetails
+import hu.bme.sch.cmsch.dto.Preview
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.security.core.Authentication
+import org.springframework.web.bind.annotation.*
+import java.util.*
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = ["\${cmsch.frontend.production-url}"], allowedHeaders = ["*"])
+@ConditionalOnBean(CommunitiesComponent::class)
+class CommunitiesApiController(
+    private val organizationService: OrganizationService
+) {
+
+    @JsonView(Preview::class)
+    @GetMapping("/community")
+    fun allCommunities(auth: Authentication): List<CommunityEntity> {
+        return organizationService.getCommunities()
+            .filter { it.visible }
+    }
+
+    @JsonView(Preview::class)
+    @GetMapping("/organization")
+    fun allOrganizations(auth: Authentication): List<OrganizationEntity> {
+        return organizationService.getOrganziations()
+            .filter { it.visible }
+    }
+
+    @JsonView(FullDetails::class)
+    @GetMapping("/community/{communityId}")
+    fun allCommunities(auth: Authentication, @PathVariable communityId: Int): CommunityEntity? {
+        return organizationService.getCommunityById(communityId)
+            .filter { it.visible }
+            .orElse(null)
+    }
+
+    @JsonView(FullDetails::class)
+    @GetMapping("/organization/{organizationId}")
+    fun allOrganizations(auth: Authentication, @PathVariable organizationId: Int): OrganizationEntity? {
+        return organizationService.getOrganziationById(organizationId)
+            .filter { it.visible }
+            .orElse(null)
+    }
+
+}
---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/communities/OrganizationService.kt
@@ -0,0 +1,35 @@
+package hu.bme.sch.cmsch.component.communities
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+import java.util.*
+
+@Service
+@ConditionalOnBean(CommunitiesComponent::class)
+open class OrganizationService(
+    private val organizationRepo: OrganizationRepository,
+    private val communityRepo: CommunityRepository
+) {
+
+    @Transactional(readOnly = true)
+    open fun getOrganziationById(id: Int): Optional<OrganizationEntity> {
+        return organizationRepo.findById(id)
+    }
+
+    @Transactional(readOnly = true)
+    open fun getCommunityById(id: Int): Optional<CommunityEntity> {
+        return communityRepo.findById(id)
+    }
+
+    @Transactional(readOnly = true)
+    open fun getOrganziations(): List<OrganizationEntity> {
+        return organizationRepo.findAll()
+    }
+
+    @Transactional(readOnly = true)
+    open fun getCommunities(): List<CommunityEntity> {
+        return communityRepo.findAll()
+    }
+
+}
