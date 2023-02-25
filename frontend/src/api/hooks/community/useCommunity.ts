b86979640be98b3afc1e49111490cad73713ee7f
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-02-25 15:14:43 +0100

    Community and organization pages now support the API

--- frontend/src/api/hooks/community/useCommunity.ts
+++ frontend/src/api/hooks/community/useCommunity.ts
@@ -5,7 +5,7 @@ import { ApiPaths } from '../../../util/paths'
 import { Community } from '../../../util/views/organization'
 import { joinPath } from '../../../util/core-functions.util'
 
-export const useCommunity = (id: string, onError: (err: any) => void) => {
+export const useCommunity = (id: string, onError?: (err: any) => void) => {
   return useQuery<Community, Error>(
     joinPath(QueryKeys.COMMUNITY, id),
     async () => {
--- frontend/src/api/hooks/community/useCommunityList.ts
+++ frontend/src/api/hooks/community/useCommunityList.ts
@@ -2,13 +2,13 @@ import axios from 'axios'
 import { useQuery } from 'react-query'
 import { QueryKeys } from '../queryKeys'
 import { ApiPaths } from '../../../util/paths'
-import { Community } from '../../../util/views/organization'
+import { CommunityListItem } from '../../../util/views/organization'
 
-export const useCommunityList = (onError: (err: any) => void) => {
-  return useQuery<Community[], Error>(
+export const useCommunityList = (onError?: (err: any) => void) => {
+  return useQuery<CommunityListItem[], Error>(
     QueryKeys.COMMUNITY,
     async () => {
-      const response = await axios.get<Community[]>(ApiPaths.COMMUNITY)
+      const response = await axios.get<CommunityListItem[]>(ApiPaths.COMMUNITY)
       return response.data
     },
     { onError: onError }
