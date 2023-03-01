334a7027e31b0268d1b87f54c769045170677514
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-03-01 10:50:50 +0100

    Already logged in bug fix

--- frontend/src/common-components/layout/CmschPage.tsx
+++ frontend/src/common-components/layout/CmschPage.tsx
@@ -2,8 +2,8 @@ import { CmschContainer, CmschContainerProps } from './CmschContainer'
 import { Navigate, Outlet } from 'react-router-dom'
 import { useAuthContext } from '../../api/contexts/auth/useAuthContext'
 import { RoleType } from '../../util/views/profile.view'
-import { Loading } from '../Loading'
 import { LoginRequired } from '../LoginRequired'
+import { LoadingPage } from '../../pages/loading/loading.page'
 
 interface CmschPageProps extends CmschContainerProps {
   loginRequired?: boolean
@@ -12,15 +12,14 @@ interface CmschPageProps extends CmschContainerProps {
 
 export const CmschPage = ({ loginRequired, children, minRole, ...props }: CmschPageProps) => {
   const { profile, profileLoading, isLoggedIn } = useAuthContext()
+  if (loginRequired) {
+    if (profileLoading) return <LoadingPage />
+    if (!isLoggedIn) return <LoginRequired />
+  }
   if (minRole && minRole > 0) {
-    if (profileLoading) {
-      return <Loading />
-    } else if (!profile?.role || RoleType[profile?.role] < minRole) {
+    if (!profile?.role || RoleType[profile?.role] < minRole) {
       return <Navigate to="/" replace />
     }
-  }
-  if (loginRequired && !isLoggedIn) {
-    return <LoginRequired />
   }
 
   return (
