40277e5159081001dc97ca0a715bdcb8cf644d31
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-07-27 20:40:49 +0200

    feat: Footer recurring logos

--- frontend/src/common-components/footer/Footer.tsx
+++ frontend/src/common-components/footer/Footer.tsx
@@ -6,18 +6,35 @@ import { customTheme } from '../../util/configs/theme.config'
 import Markdown from '../Markdown'
 import { useConfigContext } from '../../api/contexts/config/ConfigContext'
 import { useMemo } from 'react'
+import { SupporterLogo } from '../SupporterLogo'
 import parseSponsors from './utils/parseSponsors'
 
 export const Footer = () => {
   const config = useConfigContext()
   const component = config?.components.footer
   const sponsors = useMemo(() => parseSponsors(component?.sponsorLogoUrls, component?.sponsorAlts, component?.sponsorWebsiteUrls), [config])
+  if (!component) return null
+  const sponsorLabelVisible =
+    (component?.sponsorsEnabled && sponsors.length > 0) ||
+    component.bmeEnabled ||
+    component.schdesignEnabled ||
+    component.schonherzEnabled ||
+    component.vikEnabled
   return (
     <CmschContainer>
+      {component?.footerMessage && (
+        <Center flexDirection="column" mx="auto" maxWidth="100%">
+          <Markdown text={component?.footerMessage} />
+        </Center>
+      )}
+      {sponsorLabelVisible && (
+        <Text textAlign="center" mb={3} mt={10}>
+          Támogatóink
+        </Text>
+      )}
       {component?.sponsorsEnabled && sponsors.length > 0 && (
         <>
-          <Text textAlign="center">Támogatóink</Text>
-          <Flex justifyContent={'center'} alignItems="center" flexWrap="wrap">
+          <Flex justifyContent="center" alignItems="center" flexWrap="wrap">
             {sponsors.map((sp) => (
               <Link href={sp.url} m={5} key={sp.image}>
                 <Image src={sp.image} alt={sp.alt} maxHeight={40} maxWidth={40} />
@@ -26,12 +43,13 @@ export const Footer = () => {
           </Flex>
         </>
       )}
-      {component?.footerMessage && (
-        <Center flexDirection="column" px="4" py="4" mx="auto" maxWidth="100%">
-          <Markdown text={component?.footerMessage} />
-        </Center>
-      )}
-      <Flex justify="center" align="center" flexDirection={['column', null, 'row']}>
+      <HStack gap={3} mt={3} justifyContent={'center'} alignItems="center" flexWrap="wrap">
+        {component.bmeEnabled && <SupporterLogo name="bme" />}
+        {component.vikEnabled && <SupporterLogo name="vik" />}
+        {component.schonherzEnabled && <SupporterLogo name="schonherz" />}
+        {component.schdesignEnabled && <SupporterLogo name="schdesign" />}
+      </HStack>
+      <Flex mt={10} justify="center" align="center" flexDirection={['column', null, 'row']}>
         <Flex align="center" flexDirection="column" justifyContent="center" mb={10} mx={10}>
           <Image src={component?.hostLogo} maxW={40} maxH={40} my={3} alt={component?.hostAlt} />
           <Link
