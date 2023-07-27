043c86e6c1bd8dfd05b47c8a01ffffb8c614efa8
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-07-27 21:07:39 +0200

    fix: news image

--- frontend/src/pages/news/components/NewsListItem.tsx
+++ frontend/src/pages/news/components/NewsListItem.tsx
@@ -1,8 +1,9 @@
-import { Box, GridItem, Heading, HStack, Icon, LinkBox, LinkOverlay } from '@chakra-ui/react'
+import { Box, GridItem, Heading, HStack, Icon, Image, LinkBox, LinkOverlay } from '@chakra-ui/react'
 import { FaExclamation } from 'react-icons/fa'
 import { Link } from 'react-router-dom'
 import Markdown from '../../../common-components/Markdown'
-import { stringifyTimeStamp } from '../../../util/core-functions.util'
+import { API_BASE_URL } from '../../../util/configs/environment.config'
+import { joinPath, stringifyTimeStamp } from '../../../util/core-functions.util'
 import { AbsolutePaths } from '../../../util/paths'
 import { NewsArticleView } from '../../../util/views/news.view'
 
@@ -15,26 +16,45 @@ type Props = {
 const NewsListItem = ({ news, fontSize, useLink }: Props) => {
   return (
     <GridItem as={LinkBox} borderRadius="base" borderColor={news.highlighted ? 'brand.200' : 'whiteAlpha.200'} borderWidth="1px" p={4}>
-      <HStack justifyContent="space-between">
-        <Box fontSize="sm" mb={2} fontWeight={300}>
-          Közzétéve: {stringifyTimeStamp(news.timestamp)}
+      <HStack>
+        {news.imageUrl && (
+          <Image
+            borderRadius="md"
+            w={32}
+            h={32}
+            objectFit="cover"
+            objectPosition="center"
+            src={joinPath(API_BASE_URL, 'cdn', news.imageUrl)}
+            mr={3}
+          />
+        )}
+        <Box w="full">
+          <HStack justifyContent="space-between">
+            <Box fontSize="sm" mb={2} fontWeight={300}>
+              Közzétéve: {stringifyTimeStamp(news.timestamp)}
+            </Box>
+            {news.highlighted && (
+              <Box>
+                <Icon as={FaExclamation} color="brand.200" w={8} h={8} />
+              </Box>
+            )}
+          </HStack>
+          <Heading size={fontSize} my={2}>
+            {useLink ? (
+              <LinkOverlay as={Link} to={`${AbsolutePaths.NEWS}/${news.url}`}>
+                {news.title}
+              </LinkOverlay>
+            ) : (
+              news.title
+            )}
+          </Heading>
         </Box>
-        {news.highlighted && (
-          <Box>
-            <Icon as={FaExclamation} color="brand.200" w={8} h={8} />
-          </Box>
-        )}
       </HStack>
-      <Heading size={fontSize} my={2}>
-        {useLink ? (
-          <LinkOverlay as={Link} to={`${AbsolutePaths.NEWS}/${news.url}`}>
-            {news.title}
-          </LinkOverlay>
-        ) : (
-          news.title
-        )}
-      </Heading>
-      {news.briefContent && <Markdown text={news.briefContent} />}
+      {news.briefContent && (
+        <Box mt={3}>
+          <Markdown text={news.briefContent} />
+        </Box>
+      )}
     </GridItem>
   )
 }
