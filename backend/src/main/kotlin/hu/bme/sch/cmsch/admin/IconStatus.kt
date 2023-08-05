e89166d91e2c28bb3ec8ac4177b41ffd676204e9
Németh Botond <nemethbotond.sopron@gmail.com>
2023-08-05 18:52:53 +0200

    New icon introduced
    Signed-off-by: Németh Botond <nemethbotond.sopron@gmail.com>

--- backend/src/main/kotlin/hu/bme/sch/cmsch/admin/IconStatus.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/admin/IconStatus.kt
@@ -15,9 +15,16 @@ enum class IconStatus(
     QUESTION("question_mark", "#eeeeee"),
     NEW("fiber_new", "#39C392"),
     RED_FLAG("flag", "#c84848"),
+    HAPPY_FACE("sentiment_very_satisfied", "#39C392"),
+    NEUTRAL_FACE("sentiment_neutral", "#f1b962"),
+    DEAD_FACE("sentiment_very_dissatisfied", "#c84848"),
+    COLOR_WHITE("humidity_high", "#ffffff"),
+    COLOR_RED("humidity_high", "#c84848"),
+    COLOR_BLUE("humidity_high", "#0ba6c1"),
+    COLOR_YELLOW("humidity_high", "#f1b962"),
+    COLOR_BLACK("humidity_low", "#ffffff"),
     EMPTY("", "")
     ;
-
     companion object {
         @JvmStatic
         fun names() = values().map { it.name }
