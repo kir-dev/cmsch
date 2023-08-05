89e7a0fbd7fd0a3a9806f2b56e84242df71f6cd5
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-05 17:40:42 +0200

    feat: event list overhaul

--- frontend/src/pages/events/components/CardListItem.tsx
+++ frontend/src/pages/events/components/CardListItem.tsx
@@ -1,33 +1,33 @@
 import { ChevronDownIcon, ChevronUpIcon } from '@chakra-ui/icons'
-import { Box, Heading, HStack, Spacer, useColorModeValue } from '@chakra-ui/react'
+import { Box, BoxProps, Heading, HStack, Spacer, useColorModeValue } from '@chakra-ui/react'
+import { PulsingDot } from '../../../common-components/PulsingDot'
 
 type CardListItemProps = {
   title: string
   open: boolean
   toggle: () => void
+  showPulsingDot?: boolean
+  pulsingDotColor?: BoxProps['color']
 }
 
-export const CardListItem = ({ title, open, toggle }: CardListItemProps) => {
+export const CardListItem = ({ title, open, toggle, showPulsingDot, pulsingDotColor }: CardListItemProps) => {
+  const bg = useColorModeValue('#00000020', '#FFFFFF20')
+  const hoverBg = useColorModeValue('#00000030', '#FFFFFF30')
   return (
-    <Box
-      onClick={toggle}
-      borderRadius="lg"
-      padding={4}
-      backgroundColor={useColorModeValue('brand.100', 'brand.500')}
-      _hover={{ bg: useColorModeValue('brand.200', 'brand.400') }}
-      marginTop={2}
-      cursor="pointer"
-    >
+    <Box onClick={toggle} borderRadius="lg" padding={4} backgroundColor={bg} _hover={{ bg: hoverBg }} marginTop={2} cursor="pointer">
       <HStack>
         <Heading as="h3" size="md" marginY={0} maxWidth="100%">
           {title}
         </Heading>
         <Spacer />
-        {open ? (
-          <ChevronUpIcon boxSize={{ base: 5, md: 8 }} color={useColorModeValue('gray.700', 'gray.300')} />
-        ) : (
-          <ChevronDownIcon boxSize={{ base: 5, md: 8 }} color={useColorModeValue('gray.700', 'gray.300')} />
-        )}
+        <HStack>
+          {showPulsingDot && <PulsingDot color={pulsingDotColor} />}
+          {open ? (
+            <ChevronUpIcon boxSize={{ base: 5, md: 8 }} color={useColorModeValue('gray.700', 'gray.300')} />
+          ) : (
+            <ChevronDownIcon boxSize={{ base: 5, md: 8 }} color={useColorModeValue('gray.700', 'gray.300')} />
+          )}
+        </HStack>
       </HStack>
     </Box>
   )
