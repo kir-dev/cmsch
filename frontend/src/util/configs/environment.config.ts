31879fa8de671d0d3e0f6944751f8b882656f199
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-23 22:00:55 +0200

    feat: environment actualization

--- frontend/src/util/configs/environment.config.ts
+++ frontend/src/util/configs/environment.config.ts
@@ -1,9 +1,6 @@
-export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'invalid'
-export const KIRDEV_URL = process.env.REACT_APP_KIRDEV_URL || 'https://kir-dev.hu'
-export const BUGREPORT_URL = process.env.REACT_APP_BUGREPORT_URL || 'https://kir-dev.hu/about#contact'
-export const APP_NAME = process.env.REACT_APP_NAME || 'CMSch'
-export const GITHUB_ORG_URL = process.env.REACT_APP_GITHUB_ORG_URL || 'https://github.com/kir-dev/cmsch'
-export const CLIENT_BASE_URL = process.env.REACT_APP_CLIENT_BASE_URL || 'http://127.0.0.1:3000'
+export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? 'invalid'
+export const BUGREPORT_URL = process.env.REACT_APP_BUGREPORT_URL ?? 'https://kir-dev.hu/about#contact'
+export const CLIENT_BASE_URL = process.env.REACT_APP_CLIENT_BASE_URL ?? 'http://127.0.0.1:3000'
 export const OFFICIAL_LANGUAGE = process.env.REACT_APP_OFFICIAL_LANGUAGE === 'true'
 export const PASS_SERVER_URL = process.env.REACT_APP_PASS_SERVER_URL
 export const PASS_TEMPLATE = process.env.REACT_APP_PASS_TEMPLATE
