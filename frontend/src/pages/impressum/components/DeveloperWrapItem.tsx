4fcd66ec68ac5785f6c096741c3ac8e279adeba5
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-26 21:39:12 +0200

    feat: removed impressum developer border

--- frontend/src/pages/impressum/components/DeveloperWrapItem.tsx
+++ frontend/src/pages/impressum/components/DeveloperWrapItem.tsx
@@ -1,7 +1,7 @@
-import { Flex, HStack, Image, Tag, Text, useColorModeValue, WrapItem } from '@chakra-ui/react'
+import { Flex, HStack, Image, Tag, Text } from '@chakra-ui/react'
+import { Dev } from '../../../api/hooks/developers/useDevelopers'
 import { API_BASE_URL } from '../../../util/configs/environment.config'
 import { customTheme } from '../../../util/configs/theme.config'
-import { Dev } from '../../../api/hooks/developers/useDevelopers'
 
 type Props = {
   dev: Dev
@@ -9,18 +9,16 @@ type Props = {
 
 export const DeveloperWrapItem = ({ dev: { name, img, tags } }: Props) => {
   return (
-    <WrapItem border="2px" borderColor={useColorModeValue('gray.200', 'gray.700')} borderRadius="md">
-      <Flex direction="column" align="center" w="20rem" h="20rem">
-        <Text fontSize="2xl">{name}</Text>
-        <Image src={img} h="15rem" fallbackSrc={`${API_BASE_URL}/img/big_pear_logo.png`} />
-        <HStack spacing={2} my={2}>
-          {tags.map((tag) => (
-            <Tag size={'md'} variant="solid" fontWeight="bold" color="white" bgColor={customTheme.colors.kirDev} key={tag}>
-              {tag}
-            </Tag>
-          ))}
-        </HStack>
-      </Flex>
-    </WrapItem>
+    <Flex direction="column" align="center" w="20rem" h="20rem">
+      <Text fontSize="2xl">{name}</Text>
+      <Image src={img} h="15rem" fallbackSrc={`${API_BASE_URL}/img/big_pear_logo.png`} />
+      <HStack spacing={2} my={2}>
+        {tags.map((tag) => (
+          <Tag size={'md'} variant="solid" fontWeight="bold" color="white" bgColor={customTheme.colors.kirDev} key={tag}>
+            {tag}
+          </Tag>
+        ))}
+      </HStack>
+    </Flex>
   )
 }
