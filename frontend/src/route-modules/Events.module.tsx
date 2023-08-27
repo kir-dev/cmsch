8d641b14041e47823e718532842b5c89a93bfe85
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-08-27 17:30:49 +0200

    feat: calendar on separate page

--- frontend/src/route-modules/Events.module.tsx
+++ frontend/src/route-modules/Events.module.tsx
@@ -4,11 +4,13 @@ import { Paths } from '../util/paths'
 
 const EventPage = lazy(() => import('../pages/events/event.page'))
 const EventListPage = lazy(() => import('../pages/events/eventList.page'))
+const EventCalendarPage = lazy(() => import('../pages/events/eventCalendar.page'))
 
 export function EventsModule() {
   return (
     <Route path={Paths.EVENTS}>
       <Route path=":path" element={<EventPage />} />
+      <Route path={Paths.CALENDAR} element={<EventCalendarPage />} />
       <Route index element={<EventListPage />} />
     </Route>
   )
