f3c040d9d91fbfb15a2623002593cfb2985857cb
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-10 16:08:32 +0200

    feat: fixes and shape

--- frontend/src/common-components/map/MapMarker.tsx
+++ frontend/src/common-components/map/MapMarker.tsx
@@ -1,17 +1,28 @@
-import { Box, BoxProps, Center, Text, useColorModeValue, VStack } from '@chakra-ui/react'
+import { Box, BoxProps, Text, useColorModeValue, VStack } from '@chakra-ui/react'
+import { MapMarkerShape } from '../../util/views/map.view'
 
 interface MapMarkerProps {
   color?: BoxProps['color']
   text?: string
+  mapShape?: MapMarkerShape
 }
 
-export function MapMarker({ color, text }: MapMarkerProps) {
+export function MapMarker({ color, text, mapShape }: MapMarkerProps) {
+  let borderRadius: BoxProps['borderRadius'] = 'full'
+  if (mapShape === MapMarkerShape.SQUARE) borderRadius = 'md'
   const bg = useColorModeValue('white', 'gray.800')
+
   return (
     <VStack w="fit-content" spacing={1} marginLeft="calc(-50% + 10px)" marginTop="10px">
-      <Center h="20px" w="20px" borderRadius="full" bg="white" dropShadow="lg">
-        <Box h="15px" w="15px" borderRadius="full" bg={color ?? 'brand.500'} />
-      </Center>
+      <Box
+        h="20px"
+        w="20px"
+        borderRadius={borderRadius}
+        borderColor="white"
+        borderWidth="2px"
+        boxSizing="border-box"
+        bg={color ?? 'brand.500'}
+      />
       {text && (
         <Box bg={bg} py={0.5} px={2} borderRadius="full">
           <Text fontSize="xs">{text}</Text>
