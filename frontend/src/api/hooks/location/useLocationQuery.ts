b2eff42c7f1ea88159a1f6132400a2b5aa11524c
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-17 11:02:36 +0200

    feat: new location query implemented

--- frontend/src/api/hooks/location/useLocationQuery.ts
+++ frontend/src/api/hooks/location/useLocationQuery.ts
@@ -1,14 +1,14 @@
 import axios from 'axios'
 import { useQuery } from 'react-query'
-import { GroupMemberLocationView } from '../../../util/views/groupMemberLocation.view'
-import { QueryKeys } from '../queryKeys'
 import { ApiPaths } from '../../../util/paths'
+import { MapDataItemView } from '../../../util/views/map.view'
+import { QueryKeys } from '../queryKeys'
 
 export const useLocationQuery = (onError?: (err: any) => void, onSuccess?: () => void) => {
-  return useQuery<GroupMemberLocationView[], Error>(
+  return useQuery<MapDataItemView[], Error>(
     QueryKeys.LOCATIONS,
     async () => {
-      const response = await axios.get<GroupMemberLocationView[]>(ApiPaths.LOCATION)
+      const response = await axios.get<MapDataItemView[]>(ApiPaths.LOCATION)
       return response.data
     },
     { onError, onSuccess, refetchInterval: 15000 }
