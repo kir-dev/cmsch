bebc4394bfbe5c5b86f10f85f221ccf08b2ef476
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-17 11:02:36 +0200

    feat: new location query implemented

--- frontend/src/pages/map/map.page.tsx
+++ frontend/src/pages/map/map.page.tsx
@@ -1,27 +1,14 @@
 import { Checkbox, Heading, Text } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
+import { useLocationQuery } from '../../api/hooks/location/useLocationQuery'
 import { CmschPage } from '../../common-components/layout/CmschPage'
 import { MapContent } from '../../common-components/map/MapContent'
-import { MapDataItemView, MapMarkerShape } from '../../util/views/map.view'
 
 export default function MapPage() {
   const [showUserLocation, setShowUserLocation] = useState(false)
-  const mapData: MapDataItemView[] = [
-    {
-      displayName: 'Lorem ipsum dolor sit',
-      location: {
-        latitude: 47.47303,
-        longitude: 19.0531,
-        accuracy: 0
-      },
-      description: undefined,
-      marker: {
-        color: 'brand.600',
-        shape: MapMarkerShape.SQUARE
-      }
-    }
-  ]
+  const locationQuery = useLocationQuery()
+
   return (
     <CmschPage>
       <Helmet>Térkép</Helmet>
@@ -29,7 +16,7 @@ export default function MapPage() {
       <Checkbox ml={1} checked={showUserLocation} onChange={(e) => setShowUserLocation(e.target.checked)}>
         Saját pozícióm mutatása
       </Checkbox>
-      <MapContent mapData={mapData} showUserLocation={showUserLocation} />
+      <MapContent mapData={locationQuery.data ?? []} showUserLocation={showUserLocation} />
       <Text>Csak annak a helyzete látható, akinél a helymegosztás engedélyezve (használatban) van.</Text>
       <Text>A saját pozíciódat csak te látod, nem kerül megosztásra mással.</Text>
     </CmschPage>
