2afad9e466800e57286a6ea1b3813dda62d500de
Horváth István <horvath.isti@edu.bme.hu>
2023-09-20 00:13:40 +0200

    feat: change the chart used on the QR fight page to a better one

--- frontend/src/pages/qr-fight/components/LevelDataDisplay.tsx
+++ frontend/src/pages/qr-fight/components/LevelDataDisplay.tsx
@@ -1,65 +1,44 @@
 import { QrLevelDto } from '../../../util/views/qrFight.view'
 import { Box } from '@chakra-ui/react'
-import { ArcElement, Chart as ChartJS, ChartData, Legend, LegendItem, Tooltip } from 'chart.js'
-import { Doughnut } from 'react-chartjs-2'
 import { useMemo } from 'react'
-import { randomColor } from '@chakra-ui/theme-tools'
 import { useConfigContext } from '../../../api/contexts/config/ConfigContext'
 import { useColorModeValue } from '@chakra-ui/system'
+import { ResponsiveBar } from '@nivo/bar'
 
 interface LevelDataDisplayProps {
   level: QrLevelDto
 }
 
-ChartJS.register(ArcElement, Tooltip, Legend)
-
 export function LevelDataDisplay({ level }: LevelDataDisplayProps) {
   const teams = useMemo(() => {
     return Object.keys(level.teams)
-      .map((team) => ({ name: team, value: level.teams[team] }))
+      .map((team) => ({ team, value: level.teams[team] }))
       .sort((a, b) => b.value - a.value)
+      .slice(0, 5)
   }, [level.teams])
-  const colors = Object.keys(level.teams).map(() => randomColor())
   const config = useConfigContext()
-  const color = useColorModeValue(config?.components.style.lightTextColor, config?.components.style.darkTextColor)
-  const data = useMemo<ChartData<'doughnut', number[]>>(() => {
-    return {
-      labels: teams.map((t, index) => index + 1 + '. ' + t.name),
-      datasets: [
-        {
-          label: 'Elfoglalt címkék',
-          data: teams.map((t) => t.value),
-          backgroundColor: colors.map((c) => c + '60'),
-          borderColor: colors
-        }
-      ]
-    }
-  }, [])
+  const color = useColorModeValue(config.components.style.lightTextColor, config.components.style.darkTextColor)
+
+  if (!teams.length) return null
   return (
     <Box w="100%" h={300}>
-      <Doughnut
-        height="100%"
-        data={data}
-        id={level.name}
-        width="100%"
-        options={{
-          maintainAspectRatio: false,
-          plugins: {
-            legend: {
-              title: {
-                text: 'Top csapatok',
-                display: true
-              },
-              position: 'top',
-              labels: {
-                color: color,
-                filter(item: LegendItem, _: ChartData): boolean {
-                  return item.index !== undefined && item.index < 5
-                }
-              }
-            }
-          }
-        }}
+      <ResponsiveBar
+        data={teams}
+        keys={['value']}
+        indexBy="team"
+        theme={{ axis: { ticks: { text: { fill: color, fontSize: 14 } } } }}
+        labelTextColor={{ from: 'color', modifiers: [['darker', 5]] }}
+        colors={[config.components.style.lightBrandingColor]}
+        borderRadius={8}
+        padding={0.3}
+        margin={{ bottom: 45, left: 45, right: 15, top: 15 }}
+        valueScale={{ type: 'linear' }}
+        indexScale={{ type: 'band', round: true }}
+        isInteractive={false}
+        axisTop={null}
+        axisRight={null}
+        enableGridX={false}
+        enableGridY={false}
       />
     </Box>
   )
