1ae97fa631645a4faba0b1b16f674bf865691dc1
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-19 19:26:49 +0200

    fix: nullable category id

--- frontend/src/pages/teams/components/TeamTaskCategoryListItem.tsx
+++ frontend/src/pages/teams/components/TeamTaskCategoryListItem.tsx
@@ -7,21 +7,28 @@ import { TeamTaskCategoriesView } from '../../../util/views/team.view'
 export const TeamTaskCategoryListItem = ({ category }: { category: TeamTaskCategoriesView }) => {
   const bg = useOpaqueBackground(1)
   const hoverBg = useOpaqueBackground(2)
+  const innerComponent = (
+    <Flex align="center" justifyContent="space-between">
+      <Text fontWeight="bold" fontSize="xl">
+        {category.name}
+      </Text>
+      <HStack>
+        <Text fontWeight="bold">
+          {category.completed}/{category.outOf}
+        </Text>
+        <CircularProgress size={10} value={category.completed} max={category.outOf} color={'green.400'} />
+      </HStack>
+    </Flex>
+  )
+  if (typeof category.id === 'undefined' || category.id === null)
+    return (
+      <Box bg={bg} px={6} py={2} marginTop={5} borderRadius="md">
+        {innerComponent}
+      </Box>
+    )
   return (
     <Box bg={bg} px={6} py={2} marginTop={5} borderRadius="md" _hover={{ bgColor: hoverBg }}>
-      <Link to={joinPath(AbsolutePaths.TASKS, 'category', category.id)}>
-        <Flex align="center" justifyContent="space-between">
-          <Text fontWeight="bold" fontSize="xl">
-            {category.name}
-          </Text>
-          <HStack>
-            <Text fontWeight="bold">
-              {category.completed}/{category.outOf}
-            </Text>
-            <CircularProgress size={10} value={category.completed} max={category.outOf} color={'green.400'} />
-          </HStack>
-        </Flex>
-      </Link>
+      <Link to={joinPath(AbsolutePaths.TASKS, 'category', category.id)}>{innerComponent}</Link>
     </Box>
   )
 }
