8aba573cfa2faf17f40818d7efaa4e4e4e3dd16c
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-19 19:13:30 +0200

    feat: enhanced my team view with admin

--- frontend/src/pages/teams/teamAdmin.page.tsx
+++
@@ -1,7 +0,0 @@
-import { TeamDetailsCore } from './components/TeamDetailsCore'
-import { useMyTeam } from '../../api/hooks/team/queries/useMyTeam'
-
-export default function TeamAdminPage() {
-  const { data: team, isLoading, error, refetch } = useMyTeam()
-  return <TeamDetailsCore team={team} isLoading={isLoading} error={error?.message} admin refetch={refetch} />
-}
