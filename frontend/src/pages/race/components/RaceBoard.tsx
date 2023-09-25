92db310380d4f8528cbb0f16ed1dafcc85f8e4d1
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-25 22:08:35 +0200

    fix: mock removed

--- frontend/src/pages/race/components/RaceBoard.tsx
+++ frontend/src/pages/race/components/RaceBoard.tsx
@@ -7,7 +7,6 @@ import { CmschPage } from '../../../common-components/layout/CmschPage'
 import { LeaderBoardTable } from '../../../common-components/LeaderboardTable'
 import Markdown from '../../../common-components/Markdown'
 import { PageStatus } from '../../../common-components/PageStatus'
-import { LeaderBoardItemView } from '../../../util/views/leaderBoardView'
 import { RaceView } from '../../../util/views/race.view'
 
 type Props = {
@@ -21,31 +20,18 @@ const RaceBoard = ({ data, component, isError, isLoading }: Props) => {
   if (!component || !component.visible) return <ComponentUnavailable />
 
   if (isError || isLoading || !data) return <PageStatus isLoading={isLoading} isError={isError} title={component.title} />
-  let dataMock: LeaderBoardItemView[] = [
-    {
-      name: 'Kiss Béla',
-      groupName: 'Kisokos'
-    },
-    {
-      name: 'Kiss Béla',
-      groupName: 'Kisokos'
-    },
-    {
-      name: 'Kiss Béla',
-      groupName: 'Kisokos'
-    }
-  ]
+
   return (
     <CmschPage>
-      <Helmet title={data?.categoryName} />
-      <Heading mb={3}>{data?.categoryName}</Heading>
-      <Markdown text={data?.description || component.defaultCategoryDescription} />
+      <Helmet title={data.categoryName} />
+      <Heading mb={3}>{data.categoryName}</Heading>
+      <Markdown text={data.description || component.defaultCategoryDescription} />
       <Flex my={5} gap={5} flexWrap="wrap">
-        <BoardStat label="Helyezésed" value={data?.place || '-'} />
-        <BoardStat label="Legjobb időd" value={(data?.bestTime || '-') + ' mp'} />
+        <BoardStat label="Helyezésed" value={data.place || '-'} />
+        <BoardStat label="Legjobb időd" value={(data.bestTime || '-') + ' mp'} />
       </Flex>
       <Divider mb={10} />
-      <LeaderBoardTable data={dataMock} showGroup={true} suffix="mp" />
+      <LeaderBoardTable data={data.board ?? []} showGroup={true} suffix="mp" />
     </CmschPage>
   )
 }
