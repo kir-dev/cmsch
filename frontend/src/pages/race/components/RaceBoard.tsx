cd4cfec90da5cfd5e8443d50ee578b8f490b7b8d
FearsomeRover <bujgergo@gmail.com>
2023-09-25 22:54:34 +0200

    fixed raceboard

--- frontend/src/pages/race/components/RaceBoard.tsx
+++ frontend/src/pages/race/components/RaceBoard.tsx
@@ -1,4 +1,4 @@
-import { Divider, Flex, Heading } from '@chakra-ui/react'
+import { Divider, Flex, Heading, Input, InputGroup, InputLeftElement } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet-async'
 import { Race } from '../../../api/contexts/config/types'
 import { BoardStat } from '../../../common-components/BoardStat'
@@ -8,6 +8,9 @@ import { LeaderBoardTable } from '../../../common-components/LeaderboardTable'
 import Markdown from '../../../common-components/Markdown'
 import { PageStatus } from '../../../common-components/PageStatus'
 import { RaceView } from '../../../util/views/race.view'
+import { createRef, useEffect, useState } from 'react'
+import { LeaderBoardItemView } from '../../../util/views/leaderBoardView'
+import { SearchIcon } from '@chakra-ui/icons'
 
 type Props = {
   data: RaceView | undefined
@@ -17,23 +20,51 @@ type Props = {
 }
 
 const RaceBoard = ({ data, component, isError, isLoading }: Props) => {
-  if (!component || !component.visible) return <ComponentUnavailable />
+  const inputRef = createRef<HTMLInputElement>()
+  const [filteredBoard, setFilteredBoard] = useState<LeaderBoardItemView[] | undefined>(data?.board)
 
+  const handleInput = () => {
+    const search = inputRef?.current?.value.toLowerCase()
+    if (!data) {
+      setFilteredBoard(undefined)
+    } else if (!search) setFilteredBoard(data?.board)
+    else {
+      setFilteredBoard(
+        data.board?.filter((item) => {
+          return item.name.toLocaleLowerCase().includes(search)
+        })
+      )
+    }
+  }
+  useEffect(() => {
+    if (data) {
+      setFilteredBoard(data?.board)
+      if (inputRef.current) inputRef.current.value = ''
+    }
+  }, [data])
+  if (!component || !component.visible) return <ComponentUnavailable />
   if (isError || isLoading || !data) return <PageStatus isLoading={isLoading} isError={isError} title={component.title} />
 
   return (
     <CmschPage>
       <Helmet title={data.categoryName} />
       <Heading mb={3}>{data.categoryName}</Heading>
+      {component.searchEnabled && (
+        <InputGroup mt={5}>
+          <InputLeftElement h="100%">
+            <SearchIcon />
+          </InputLeftElement>
+          <Input ref={inputRef} placeholder="Keresés..." size="lg" onChange={handleInput} autoFocus={true} />
+        </InputGroup>
+      )}
       <Markdown text={data.description || component.defaultCategoryDescription} />
       <Flex my={5} gap={5} flexWrap="wrap">
         <BoardStat label="Helyezésed" value={data.place || '-'} />
         <BoardStat label="Legjobb időd" value={(data.bestTime || '-') + ' mp'} />
       </Flex>
       <Divider mb={10} />
-      <LeaderBoardTable data={data.board ?? []} showGroup={true} suffix="mp" />
+      <LeaderBoardTable data={filteredBoard || []} showGroup={true} suffix="mp" />
     </CmschPage>
   )
 }
-
 export default RaceBoard
