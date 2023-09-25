0919cacac9e7c4e3920e7eeabd69093bd3e09335
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-25 22:06:41 +0200

    feat: menu dropdown bg improved

--- frontend/src/common-components/navigation/desktop/NavItemWithChildren.tsx
+++ frontend/src/common-components/navigation/desktop/NavItemWithChildren.tsx
@@ -9,33 +9,36 @@ type Props = {
   menu: Menu
 }
 
-export const NavItemWithChildren = ({ menu }: Props) => (
-  <Box key={menu.name} p={2}>
-    <Popover trigger="hover" placement="bottom-start">
-      <PopoverTrigger>
-        <Box>
-          <LinkComponent url={menu.url || '#'} external={menu.external}>
-            <HStack
-              _hover={{
-                textDecoration: 'none',
-                color: useColorModeValue('brand.500', 'brand.400')
-              }}
-            >
-              <chakra.span fontSize="md" fontWeight={500}>
-                {menu.name}
-              </chakra.span>
-              <Icon as={FaChevronDown} w={4} h={4} m={0} />
-            </HStack>
-          </LinkComponent>
-        </Box>
-      </PopoverTrigger>
-      <PopoverContent border={0} boxShadow="xl" bg={useColorModeValue('gray.50', 'gray.800')} p={4} rounded="xl" maxW="2xs">
-        <Stack>
-          {menu.children.map((child) => (
-            <ChildNavItem key={child.name} menu={child} />
-          ))}
-        </Stack>
-      </PopoverContent>
-    </Popover>
-  </Box>
-)
+export const NavItemWithChildren = ({ menu }: Props) => {
+  const bg = useColorModeValue('darkContainerColor.600', 'darkContainerColor.600')
+  return (
+    <Box key={menu.name} p={2}>
+      <Popover trigger="hover" placement="bottom-start">
+        <PopoverTrigger>
+          <Box>
+            <LinkComponent url={menu.url || '#'} external={menu.external}>
+              <HStack
+                _hover={{
+                  textDecoration: 'none',
+                  color: useColorModeValue('brand.500', 'brand.400')
+                }}
+              >
+                <chakra.span fontSize="md" fontWeight={500}>
+                  {menu.name}
+                </chakra.span>
+                <Icon as={FaChevronDown} w={4} h={4} m={0} />
+              </HStack>
+            </LinkComponent>
+          </Box>
+        </PopoverTrigger>
+        <PopoverContent border={0} boxShadow="xl" bg={bg} p={4} rounded="xl" maxW="2xs">
+          <Stack>
+            {menu.children.map((child) => (
+              <ChildNavItem key={child.name} menu={child} />
+            ))}
+          </Stack>
+        </PopoverContent>
+      </Popover>
+    </Box>
+  )
+}
