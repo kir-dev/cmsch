1eca706fa90ca1f1b16b6617ef6957730bc547f3
Bálint Berente <30603208+berenteb@users.noreply.github.com>
2023-09-27 18:12:43 +0200

    feat: search bar improvements

--- frontend/src/pages/teams/teamList.page.tsx
+++ frontend/src/pages/teams/teamList.page.tsx
@@ -1,59 +1,32 @@
-import { createRef, useMemo, useState } from 'react'
+import { Heading } from '@chakra-ui/react'
 import { Helmet } from 'react-helmet-async'
-import { CloseIcon, SearchIcon } from '@chakra-ui/icons'
-import { Heading, Input, InputGroup, InputLeftElement, InputRightElement } from '@chakra-ui/react'
+import { useConfigContext } from '../../api/contexts/config/ConfigContext'
+import { useTeamList } from '../../api/hooks/team/queries/useTeamList'
+import { ComponentUnavailable } from '../../common-components/ComponentUnavailable'
 
 import { CmschPage } from '../../common-components/layout/CmschPage'
-import { TeamListItem } from './components/TeamListItem'
-import { useTeamList } from '../../api/hooks/team/queries/useTeamList'
-import { useConfigContext } from '../../api/contexts/config/ConfigContext'
-import { ComponentUnavailable } from '../../common-components/ComponentUnavailable'
 import { PageStatus } from '../../common-components/PageStatus'
+import { SearchBar } from '../../common-components/SearchBar'
+import { useSearch } from '../../util/useSearch'
+import { TeamListItemView } from '../../util/views/team.view'
+import { TeamListItem } from './components/TeamListItem'
 
 export default function TeamListPage() {
   const config = useConfigContext()
   const component = config?.components.team
   const { data, isLoading, isError } = useTeamList()
-  const [search, setSearch] = useState('')
-  const filteredTeams = useMemo(() => {
-    return data?.filter((c) => {
-      return c.name.toLocaleLowerCase().includes(search)
-    })
-  }, [data, search])
-  const inputRef = createRef<HTMLInputElement>()
+  const searchArgs = useSearch<TeamListItemView>(data ?? [], (item, search) => item.name.toLowerCase().includes(search))
 
   if (!component) return <ComponentUnavailable />
 
   if (isError || isLoading || !data) return <PageStatus isLoading={isLoading} isError={isError} title={component.title} />
 
-  const handleInput = () => {
-    const searchFieldValue = inputRef?.current?.value.toLowerCase()
-    if (!searchFieldValue) setSearch('')
-    else setSearch(searchFieldValue)
-  }
-
   return (
     <CmschPage>
       <Helmet title={component.title} />
       <Heading>{component.title}</Heading>
-      <InputGroup mt={5}>
-        <InputLeftElement h="100%">
-          <SearchIcon />
-        </InputLeftElement>
-        <Input ref={inputRef} placeholder="Keresés..." size="lg" onChange={handleInput} autoFocus={true} />
-        {search && (
-          <InputRightElement
-            h="100%"
-            onClick={() => {
-              setSearch('')
-              if (inputRef.current?.value) inputRef.current.value = ''
-            }}
-          >
-            <CloseIcon />
-          </InputRightElement>
-        )}
-      </InputGroup>
-      {filteredTeams?.map((team) => (
+      <SearchBar mt={5} {...searchArgs} />
+      {searchArgs.filteredData?.map((team) => (
         <TeamListItem key={team.id} team={team} detailEnabled={component?.showTeamDetails} />
       ))}
     </CmschPage>
