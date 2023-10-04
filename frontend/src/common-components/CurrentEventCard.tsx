dc3a6b6c4e84bea0c28e27729d5a68a8e5905d4a
FearsomeRover <bujgergo@gmail.com>
2023-10-04 21:39:35 +0200

    moved card to header

--- frontend/src/common-components/CurrentEventCard.tsx
+++ frontend/src/common-components/CurrentEventCard.tsx
@@ -1,41 +1,84 @@
-import { Alert, AlertDescription, AlertIcon, HStack } from '@chakra-ui/react'
+import { Alert, AlertDescription, AlertIcon, Box, Flex, HStack, useColorMode, useColorModeValue } from '@chakra-ui/react'
 import { useEventListQuery } from '../api/hooks/event/useEventListQuery'
 import { isCurrentEvent } from '../util/core-functions.util'
 import { AbsolutePaths } from '../util/paths'
 import { Link } from 'react-router-dom'
+import { CmschContainer } from './layout/CmschContainer'
+import { useConfigContext } from '../api/contexts/config/ConfigContext'
+import { useOpaqueBackground } from '../util/core-functions.util'
+import { PulsingDot } from './PulsingDot'
+import { relative } from 'path'
 
 export default function CurrentEventCard() {
+  const config = useConfigContext()
   const { data, error } = useEventListQuery()
   if (!data || error) return null
   const currentEvents = data.filter((event) => isCurrentEvent(event))
   if (currentEvents.length === 0) return null
   return (
-    <Alert
+    <Flex
+      color={useColorModeValue('brand.800', 'white')}
+      py={{ base: 2 }}
+      px={{ base: 6 }}
+      bg={useOpaqueBackground(1)}
+      textAlign={'left'}
       borderRadius={[0, null, 'xl']}
-      opacity={1}
-      status={'info'}
-      variant="solid"
-      mx="auto"
-      w="100%"
-      maxWidth={['100%', '64rem']}
-      mb={5}
+      m="2"
+      direction={'row'}
+      justify="center"
     >
-      <HStack justify="left" flex={1}>
-        <AlertIcon />
-        <AlertDescription wordBreak="break-word">
-          <p>
-            A{' '}
-            <b>
-              {currentEvents.map((event, idx) => (
-                <Link key={event.url} to={`${AbsolutePaths.EVENTS}/${event.url}`}>
-                  {event.title + (idx == currentEvents.length - 1 ? ' ' : ', ')}
-                </Link>
-              ))}
-            </b>
-            {currentEvents.length == 1 ? 'esemény éppen most zajlik' : 'események éppen most zajlanak.'}
-          </p>
-        </AlertDescription>
-      </HStack>
-    </Alert>
+      <PulsingDot color={'green.300'} mr="4" />
+      <Box position={'relative'} bottom="0px">
+        A{' '}
+        <b>
+          {currentEvents.map((event, idx) => (
+            <Link key={event.url} to={`${AbsolutePaths.EVENTS}/${event.url}`}>
+              {event.title + (idx == currentEvents.length - 1 ? ' ' : ', ')}
+            </Link>
+          ))}
+        </b>
+        {currentEvents.length == 1 ? 'esemény éppen most zajlik' : 'események éppen most zajlanak.'}
+      </Box>
+    </Flex>
   )
+  // return (
+  //   <Flex
+  //     color={useColorModeValue('brand.800', 'white')}
+  //     minH={{ base: '3rem', md: '4.5rem' }}
+  //     maxW={['100%', '100%', '56rem', '72rem']}
+  //     py={{ base: 2 }}
+  //     px={{ base: 4 }}
+  //     align="center"
+  //   >
+
+  //   </Flex>
+
+  // <Alert
+  //   borderRadius={[0, null, 'xl']}
+  //   opacity={1}
+  //   status={'info'}
+  //   variant="solid"
+  //   mx="auto"
+  //   w="100%"
+  //   maxWidth={['100%', '64rem']}
+  //   mb={5}
+  // >
+  //   <HStack justify="left" flex={1}>
+  //     <AlertIcon />
+  //     <AlertDescription wordBreak="break-word">
+  //       <p>
+  //         A{' '}
+  //         <b>
+  //           {currentEvents.map((event, idx) => (
+  //             <Link key={event.url} to={`${AbsolutePaths.EVENTS}/${event.url}`}>
+  //               {event.title + (idx == currentEvents.length - 1 ? ' ' : ', ')}
+  //             </Link>
+  //           ))}
+  //         </b>
+  //         {currentEvents.length == 1 ? 'esemény éppen most zajlik' : 'események éppen most zajlanak.'}
+  //       </p>
+  //     </AlertDescription>
+  //   </HStack>
+  // </Alert>
+  //)
 }
