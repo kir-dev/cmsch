95d6bede4511f6c7e0b271164429df7f4072769e
Gerviba <gerviba@sch.bme.hu>
2023-11-17 15:34:54 +0100

    Fix profile frontend

--- frontend/src/pages/profile/profile.page.tsx
+++ frontend/src/pages/profile/profile.page.tsx
@@ -142,7 +142,7 @@ const ProfilePage = ({}: Props) => {
         </>
       )}
 
-      {component.showQr && <ProfileQR profile={profile} component={component} />}
+      {component.showQr && profile.cmschId && <ProfileQR profile={profile} component={component} />}
       {(component.showTasks || component.showRiddles || component.showTokens) && <Divider my={10} borderWidth={2} />}
       <Flex justify="center" alignItems="center" flexWrap="wrap">
         {component.showTasks && (
