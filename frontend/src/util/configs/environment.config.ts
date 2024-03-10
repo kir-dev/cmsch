52f2cfb12e7846e98e17447c3ec368efaba6df6d
Sámuel Fekete <feketesamu@gmail.com>
2024-03-10 19:34:34 +0100

    fix bugreport url

--- frontend/src/util/configs/environment.config.ts
+++ frontend/src/util/configs/environment.config.ts
@@ -1,5 +1,5 @@
 export const API_BASE_URL = process.env.REACT_APP_API_BASE_URL ?? 'invalid'
-export const BUGREPORT_URL = process.env.REACT_APP_BUGREPORT_URL ?? 'https://kir-dev.hu/about#contact'
+export const BUGREPORT_URL = process.env.REACT_APP_BUGREPORT_URL ?? 'https://kir-dev.hu/about/contact'
 export const CLIENT_BASE_URL = process.env.REACT_APP_CLIENT_BASE_URL ?? 'http://127.0.0.1:3000'
 export const OFFICIAL_LANGUAGE = process.env.REACT_APP_OFFICIAL_LANGUAGE === 'true'
 export const PASS_SERVER_URL = process.env.REACT_APP_PASS_SERVER_URL
