fe85b4603ba851cc9c841f118508d78d249233bf
Horváth István <horvath.isti@edu.bme.hu>
2024-03-13 22:43:53 +0100

    Parameterize hardcoded strings in attendance report PDF

--- backend/src/main/kotlin/hu/bme/sch/cmsch/util/Utility.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/util/Utility.kt
@@ -7,11 +7,14 @@ import hu.bme.sch.cmsch.service.UserService
 import org.commonmark.node.Node
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
+import org.springframework.core.io.ClassPathResource
 import org.springframework.security.core.Authentication
 import org.springframework.stereotype.Component
 import org.springframework.web.multipart.MultipartFile
 import java.io.File
 import java.io.IOException
+import java.nio.file.Files
+import java.nio.file.Paths
 import java.util.*
 
 
@@ -29,15 +32,17 @@ class DI(
     }
 }
 
+private fun getFileStoragePath(): String = if (!DI.instance.startupPropertyConfig.external.startsWith("/")) {
+    System.getProperty("user.dir") + "/" + DI.instance.startupPropertyConfig.external
+} else {
+    DI.instance.startupPropertyConfig.external
+}
+
 fun MultipartFile.uploadFile(target: String, overrideName: String? = null): String? {
     if (this.isEmpty || this.contentType == null)
         return null
 
-    var path = if (!DI.instance.startupPropertyConfig.external.startsWith("/")) {
-        System.getProperty("user.dir") + "/" + DI.instance.startupPropertyConfig.external
-    } else {
-        DI.instance.startupPropertyConfig.external
-    }
+    var path = getFileStoragePath()
     val dir = File(path, target)
     dir.mkdirs()
     val originalFilename = this.originalFilename ?: ""
@@ -68,6 +73,18 @@ fun Authentication.getUserEntityFromDatabase(): UserEntity {
 
 fun Authentication?.getUserEntityFromDatabaseOrNull(): UserEntity? {
     return if (this == null) null else DI.instance.userService.findById(this.name).orElse(null)
+}
+
+fun readAsset(assetName: String): Optional<ByteArray> {
+    try {
+        return Optional.of(ClassPathResource(assetName).inputStream.readAllBytes())
+    } catch (ignored: Throwable) {
+    }
+    try {
+        return Optional.of(Files.readAllBytes(Paths.get(getFileStoragePath(), assetName)))
+    } catch (ignored: Throwable) {
+    }
+    return Optional.empty()
 }
 
 fun markdownToHtml(markdown: String): String {
