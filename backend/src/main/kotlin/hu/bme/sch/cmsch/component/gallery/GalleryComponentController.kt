3e0c4fbb9ef5cab4e4c90868749798fb8afea4d9
Albert Ragány-Németh <me@alb1.hu>
2024-04-05 22:24:21 +0200

    Add gallery component backend

---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/gallery/GalleryComponentController.kt
@@ -0,0 +1,29 @@
+package hu.bme.sch.cmsch.component.gallery
+
+import hu.bme.sch.cmsch.component.ComponentApiBase
+import hu.bme.sch.cmsch.component.app.MenuService
+import hu.bme.sch.cmsch.service.AdminMenuService
+import hu.bme.sch.cmsch.service.AuditLogService
+import hu.bme.sch.cmsch.service.ControlPermissions.PERMISSION_CONTROL_GALLERY
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/admin/control/component/gallery")
+@ConditionalOnBean(GalleryComponent::class)
+class GalleryComponentController(
+    adminMenuService: AdminMenuService,
+    component: GalleryComponent,
+    menuService: MenuService,
+    auditLogService: AuditLogService
+) : ComponentApiBase(
+    adminMenuService,
+    GalleryComponent::class.java,
+    component,
+    PERMISSION_CONTROL_GALLERY,
+    "Galéria",
+    "Galéria testreszabása",
+    menuService = menuService,
+    auditLogService = auditLogService
+)
