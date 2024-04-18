349461db0a014840ad92672bf741a2889709f236
Horváth István <horvath.isti@edu.bme.hu>
2024-04-18 21:44:35 +0200

    Display a message if a team is not visible or not playing

--- frontend/src/pages/teams/teamDetails.page.tsx
+++ frontend/src/pages/teams/teamDetails.page.tsx
@@ -3,10 +3,17 @@ import { Navigate, useParams } from 'react-router-dom'
 import { useTeamDetails } from '../../api/hooks/team/queries/useTeamDetails'
 import { TeamDetailsCore } from './components/TeamDetailsCore'
 import { AbsolutePaths } from '../../util/paths'
+import { TeamStatus } from '../../util/views/team.view'
+import { TeamIsNotPlaying } from './components/TeamIsNotPlaying'
 
 export default function TeamDetailsPage() {
   const { id } = useParams()
-  const { data: team, isLoading, error, refetch } = useTeamDetails(id)
+  const { data: optionalTeam, isLoading, error, refetch } = useTeamDetails(id)
+  if (optionalTeam?.status === TeamStatus.NOT_VISIBLE) {
+    return <TeamIsNotPlaying />
+  }
+
+  const team = optionalTeam?.team
   if (team?.ownTeam) return <Navigate to={AbsolutePaths.MY_TEAM} replace={true} />
   return <TeamDetailsCore team={team} isLoading={isLoading} error={error?.message} refetch={refetch} />
 }
