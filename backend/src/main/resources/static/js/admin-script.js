3b426d121cb64d96f1dd15f65ddfbe5512f2090c
Horváth István <horvath.isti@edu.bme.hu>
2024-04-26 12:32:17 +0200

    Remove unused assets and templates

--- backend/src/main/resources/static/js/admin-script.js
+++
@@ -1,4 +0,0 @@
-function toggleCollapse(menuId) {
-    document.getElementById(`menu${menuId}`).classList.toggle('collapsed')
-    localStorage.setItem(`menu${menuId}`, document.getElementById(`menu${menuId}`).classList.contains('collapsed') ? 'false' : 'true');
-}
