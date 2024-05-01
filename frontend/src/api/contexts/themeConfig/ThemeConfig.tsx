f076515e3f120d2be78eca6892f2034e875c6b97
Horváth István <horvath.isti@edu.bme.hu>
2024-05-01 23:04:56 +0200

    Fix background jump when scrolling on mobile

--- frontend/src/api/contexts/themeConfig/ThemeConfig.tsx
+++ frontend/src/api/contexts/themeConfig/ThemeConfig.tsx
@@ -2,7 +2,6 @@ import { customTheme } from '../../../util/configs/theme.config'
 import { useConfigContext } from '../config/ConfigContext'
 import { ChakraProvider, useColorMode } from '@chakra-ui/react'
 import { PropsWithChildren, useMemo } from 'react'
-import { mode } from '@chakra-ui/theme-tools'
 import { getColorShadesForColor } from '../../../util/core-functions.util'
 
 export const ThemeConfig = ({ children }: PropsWithChildren) => {
@@ -17,25 +16,6 @@ export const ThemeConfig = ({ children }: PropsWithChildren) => {
       customTheme.colors.darkContainerColor = getColorShadesForColor(config.components.style.darkContainerColor)
       customTheme.colors.darkContainerBg = config.components.style.darkContainerColor
       setColorMode((config.components.style.deviceTheme && 'system') || (config.components.style.forceDarkMode && 'dark') || 'light')
-      const defaultGlobal = customTheme.styles.global
-      customTheme.styles.global = (props: any) => ({
-        ...defaultGlobal(props),
-        body: {
-          bg: mode(config.components.style.lightBackgroundColor, config.components.style.darkBackgroundColor)(props),
-          color: mode(config.components.style.lightTextColor, config.components.style.darkTextColor)(props),
-          bgImage: mode(`url(${config.components.style.lightBackgroundUrl})`, `url(${config.components.style.darkBackgroundUrl})`)(props),
-          bgRepeat: 'no-repeat',
-          bgSize: 'cover',
-          bgPosition: 'center',
-          bgAttachment: 'fixed',
-          [`@media screen and (max-width: ${props.theme.breakpoints.sm})`]: {
-            bgImage: mode(
-              `url(${config.components.style.lightMobileBackgroundUrl})`,
-              `url(${config.components.style.darkMobileBackgroundUrl})`
-            )
-          }
-        }
-      })
       customTheme.fonts = {
         heading: config.components.style.displayFontName,
         body: config.components.style.mainFontName,
