78b2ad64e48cf76f1add2889a817db51a336d69e
Horváth István <horvath.isti@edu.bme.hu>
2024-05-01 23:04:56 +0200

    Fix background jump when scrolling on mobile

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -8,25 +8,28 @@ import { EnabledModules, GetRoutesForModules } from './util/configs/modules.conf
 import CountdownPage from './pages/countdown/countdown.page'
 import IndexPage from './pages/index/index.page'
 import { l } from './util/language'
+import { AppBackground } from './common-components/layout/AppBackground'
 
 export function App() {
   return (
-    <CountdownPage>
-      <CmschLayout>
-        <Suspense>
-          <Routes>
-            <Route path="/">
-              {GetRoutesForModules(EnabledModules)}
-              <Route index element={<IndexPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="logout" element={<HomePage />} />
-              {/** Error handling pages */}
-              <Route path="error" element={<ErrorPage />} />
-              <Route path="*" element={<ErrorPage message={l('not-found-message')} />} />
-            </Route>
-          </Routes>
-        </Suspense>
-      </CmschLayout>
-    </CountdownPage>
+    <AppBackground>
+      <CountdownPage>
+        <CmschLayout>
+          <Suspense>
+            <Routes>
+              <Route path="/">
+                {GetRoutesForModules(EnabledModules)}
+                <Route index element={<IndexPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="logout" element={<HomePage />} />
+                {/** Error handling pages */}
+                <Route path="error" element={<ErrorPage />} />
+                <Route path="*" element={<ErrorPage message={l('not-found-message')} />} />
+              </Route>
+            </Routes>
+          </Suspense>
+        </CmschLayout>
+      </CountdownPage>
+    </AppBackground>
   )
 }
---
+++ frontend/src/common-components/layout/AppBackground.tsx
@@ -0,0 +1,30 @@
+import { FC, PropsWithChildren } from 'react'
+import { Box, useColorModeValue } from '@chakra-ui/react'
+import { useConfigContext } from '../../api/contexts/config/ConfigContext'
+
+export const AppBackground: FC<PropsWithChildren> = ({ children }) => {
+  const config = useConfigContext()
+  const textColor = useColorModeValue(config.components.style.lightTextColor, config.components.style.darkTextColor)
+  const background = useColorModeValue(config.components.style.lightBackgroundColor, config.components.style.darkBackgroundColor)
+  const backgroundImage = useColorModeValue(
+    `url(${config.components.style.lightBackgroundUrl})`,
+    `url(${config.components.style.darkBackgroundUrl})`
+  )
+  return (
+    <>
+      <Box
+        position="fixed"
+        zIndex={-9999999}
+        height="100vh"
+        width="100vw"
+        bg={background}
+        color={textColor}
+        bgImage={backgroundImage}
+        bgRepeat={'no-repeat'}
+        bgSize={'cover'}
+        bgPosition={'center'}
+      ></Box>
+      {children}
+    </>
+  )
+}
