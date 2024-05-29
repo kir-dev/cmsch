a54cbff99405602713c9c9b0eaaa1504b61c5edd
Horváth István <horvath.isti@edu.bme.hu>
2024-05-29 15:42:48 +0200

    Fix getting background notifications twice

--- frontend/public/firebase-messaging-sw.js
+++ frontend/public/firebase-messaging-sw.js
@@ -3,17 +3,6 @@ importScripts('https://www.gstatic.com/firebasejs/8.10.0/firebase-messaging.js')
 
 const urlParams = new URLSearchParams(location.search)
 const firebaseConfig = Object.fromEntries(urlParams.entries())
-firebase.initializeApp(firebaseConfig)
 
-if (firebase.messaging.isSupported()) {
-  const messaging = firebase.messaging()
-  messaging.onBackgroundMessage((payload) => {
-    const notificationTitle = payload.notification.title
-    const notificationOptions = {
-      body: payload.notification.body,
-      tag: notificationTitle,
-      icon: payload.notification?.image
-    }
-    self.registration.showNotification(notificationTitle, notificationOptions)
-  })
-}
+firebase.initializeApp(firebaseConfig)
+firebase.messaging() // Listen to incoming notifications by initializing messaging
