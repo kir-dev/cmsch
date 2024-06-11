ea78efd32c5ed0485f75d188032de9fd1b61ed21
Horváth István <horvath.isti@edu.bme.hu>
2024-06-11 23:10:01 +0200

    Add missing entity configuration

---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/pushnotification/PushNotificationComponentEntityConfiguration.kt
@@ -0,0 +1,10 @@
+package hu.bme.sch.cmsch.component.pushnotification
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ConditionalOnBean(PushNotificationComponent::class)
+@EntityScan(basePackageClasses = [PushNotificationComponent::class])
+class PushNotificationComponentEntityConfiguration
