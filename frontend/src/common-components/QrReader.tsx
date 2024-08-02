e3e188ceb6d58577bca7fe217c986d2bbfb3f004
Albert Ragány-Németh <me@alb1.hu>
2024-08-02 22:53:02 +0200

    Fix camera being left on after leaving QR page

--- frontend/src/common-components/QrReader.tsx
+++ frontend/src/common-components/QrReader.tsx
@@ -8,27 +8,34 @@ interface QrReaderProps {
 export function QrReader({ onScan }: QrReaderProps) {
   const codeReader = useRef(new BrowserQRCodeReader())
   const videoElement = useRef<HTMLVideoElement>(null)
-  const controls = useRef<IScannerControls>()
-
-  async function decodeContinuously() {
-    if (!videoElement.current) return
-    const videoInputDevices = await BrowserCodeReader.listVideoInputDevices()
-    const selectedDeviceId = videoInputDevices[0].deviceId
-    controls.current = await codeReader.current.decodeFromVideoDevice(selectedDeviceId, videoElement.current, (result, err, controls) => {
-      console.debug(result, err)
-      if (result) {
-        controls.stop()
-        onScan(result.getText())
-      }
-    })
-  }
 
   useEffect(() => {
-    decodeContinuously()
-    return () => {
-      controls.current?.stop()
+    let controls: IScannerControls | null
+    let cancelled = false
+
+    async function decodeContinuously() {
+      if (!videoElement.current) return
+      const videoInputDevices = await BrowserCodeReader.listVideoInputDevices()
+      const selectedDeviceId = videoInputDevices[0].deviceId
+      if (cancelled) return
+      controls = await codeReader.current.decodeFromVideoDevice(selectedDeviceId, videoElement.current, (result, err, controls) => {
+        console.debug(result, err)
+        if (result) {
+          controls.stop()
+          onScan(result.getText())
+        }
+      })
+
+      if (cancelled) controls.stop()
     }
-  }, [])
+
+    decodeContinuously()
+
+    return () => {
+      cancelled = true
+      controls?.stop()
+    }
+  }, [onScan, videoElement])
   return (
     <div className=" w-fit h-fit rounded-lg overflow-hidden mx-auto">
       <video height="100%" width="100%" className="object-cover" ref={videoElement} />
