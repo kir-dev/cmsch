468a2bda0ad267a93fb30e96465849dcf8291596
FearsomeRover <bujgergo@gmail.com>
2024-08-09 18:40:43 +0200

    Added new admin panel for viewing token counts by group

---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/token/TokenAdminTokensByTeamController.kt
@@ -0,0 +1,66 @@
+package hu.bme.sch.cmsch.component.token
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import hu.bme.sch.cmsch.controller.admin.OneDeepEntityPage
+import hu.bme.sch.cmsch.repository.ManualRepository
+import hu.bme.sch.cmsch.service.*
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Controller
+import org.springframework.transaction.PlatformTransactionManager
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
+@RequestMapping("/admin/control/token-properties-team")
+@ConditionalOnBean(TokenComponent::class)
+@ConditionalOnProperty(
+    prefix = "hu.bme.sch.cmsch.startup",
+    name = ["token-ownership-mode"],
+    havingValue = "USER",
+    matchIfMissing = false
+)
+public class TokenAdminTokensByTeamController(
+    private val repo: TokenPropertyRepository,
+    importService: ImportService,
+    adminMenuService: AdminMenuService,
+    component: TokenComponent,
+    auditLog: AuditLogService,
+    objectMapper: ObjectMapper,
+    transactionManager: PlatformTransactionManager,
+    env: Environment
+): OneDeepEntityPage<UserGroupTokenCount>(
+    "token-properties-team",
+    UserGroupTokenCount::class, ::UserGroupTokenCount,
+    "Csoportos tokenek", "Csoportos tokenek",
+    "Tokenek csoportonként csoportosítva",
+
+    transactionManager,
+    object : ManualRepository<UserGroupTokenCount, Int>() {
+        override fun findAll(): Iterable<UserGroupTokenCount> {
+            return repo.countByAllUserGroup()
+        }
+
+    },
+
+    importService,
+    adminMenuService,
+    component,
+    auditLog,
+    objectMapper,
+    env,
+
+    showPermission =   StaffPermissions.PERMISSION_SHOW_TOKEN_SUBMISSIONS,
+    createPermission = ImplicitPermissions.PERMISSION_NOBODY,
+    editPermission =   ImplicitPermissions.PERMISSION_NOBODY,
+    deletePermission = ImplicitPermissions.PERMISSION_NOBODY,
+
+    createEnabled = false,
+    editEnabled   = false,
+    deleteEnabled = false,
+    importEnabled = false,
+    exportEnabled = false,
+
+    adminMenuIcon = "local_activity",
+    adminMenuPriority = 3,
+)
