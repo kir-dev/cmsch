d750c6713916930b8c685c647bf11d220ed33f85
FearsomeRover <bujgergo@gmail.com>
2024-08-10 11:35:49 +0200

    Changed admin leaderboard fields

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/token/UserGroupTokenCount.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/token/UserGroupTokenCount.kt
@@ -2,16 +2,27 @@ package hu.bme.sch.cmsch.component.token
 
 import hu.bme.sch.cmsch.admin.GenerateOverview
 import hu.bme.sch.cmsch.admin.OVERVIEW_TYPE_ID
+import hu.bme.sch.cmsch.admin.OVERVIEW_TYPE_NUMBER
 import hu.bme.sch.cmsch.model.IdentifiableEntity
 
-class UserGroupTokenCount (
+class UserGroupTokenCount(
     @property:GenerateOverview(renderer = OVERVIEW_TYPE_ID, columnName = "Csoport ID", order = -1)
     override var id: Int = 0,
     @property:GenerateOverview(columnName = "Csoport név", order = 1)
     var groupName: String = "",
-    @property:GenerateOverview(columnName = "Tokenek száma", order = 3)
+    @property:GenerateOverview(columnName = "Tokenek száma", order = 4, renderer = OVERVIEW_TYPE_NUMBER)
     var tokenCount: Long = 0,
-    @property:GenerateOverview(columnName = "Csoporttagok száma", order = 4)
-    var memberCount: Long = 0
+    @property:GenerateOverview(columnName = "Tokenek összpontszáma", order = 3, renderer = OVERVIEW_TYPE_NUMBER)
+    var tokenPoints: Long = 0,
+    @property:GenerateOverview(columnName = "Csoporttagok száma", order = 5, renderer = OVERVIEW_TYPE_NUMBER)
+    var memberCount: Int = 0,
 
-): IdentifiableEntity
+
+    ) : IdentifiableEntity {
+    @property:GenerateOverview(columnName = "Korrigált pontok", order = 2, renderer = OVERVIEW_TYPE_NUMBER)
+    var correctedPoints: Int? = null
+
+    init {
+        correctedPoints = if (memberCount != 0) (tokenPoints / memberCount).toInt() else null
+    }
+}
