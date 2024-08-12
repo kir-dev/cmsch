11033d3ea663843f4c294d50b8c64a29ace7b0bc
Horváth István <horvath.isti@edu.bme.hu>
2024-08-12 13:41:48 +0200

    Update token generator

--- tools/qr-generator/src/generator/codes.js
+++ tools/qr-generator/src/generator/codes.js
@@ -1,3 +1,5 @@
+import ProgressBar from 'progress'
+
 const SafeChars = [...'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789']
 
 const generateCode = (length) =>
@@ -8,12 +10,22 @@ const generateCode = (length) =>
 export const generateUniqueCodes = ({ count, prefix, length }) => {
   const codes = new Set()
 
-  for (let i = 0; codes.size < count /* <- unusual predicate */; i++) {
-    if (i > count * 100) {
+  console.log("Tokenek generálása...")
+  const bar = new ProgressBar(':bar :current/:total :percent', { total: count })
+
+  let attempt = 0
+  while (codes.size < count) {
+    attempt++
+
+    if (attempt > count * 100) {
       throw new Error('Nem sikerült elegendő egyedi kódot készíteni, növeld a lehetséges hosszt!')
     }
 
-    codes.add(prefix + generateCode(length - prefix.length))
+    const newCode = prefix + generateCode(length - prefix.length)
+    const oldCount = codes.size
+    codes.add(newCode)
+
+    if (oldCount < codes.size) bar.tick()
   }
 
   return [...codes]
