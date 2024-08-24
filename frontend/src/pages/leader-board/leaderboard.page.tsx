5fc9ab54226fcd2ce68314e2fc950da7e91966d8
Albert Ragány-Németh <me@alb1.hu>
2024-08-24 18:46:26 +0200

    Add navigation support to leaderboard tabs

--- frontend/src/pages/leader-board/leaderboard.page.tsx
+++ frontend/src/pages/leader-board/leaderboard.page.tsx
@@ -4,21 +4,40 @@ import { useConfigContext } from '../../api/contexts/config/ConfigContext'
 import { useLeaderBoardQuery } from '../../api/hooks/leaderboard/useLeaderBoardQuery'
 import { BoardStat } from '../../common-components/BoardStat'
 import { ComponentUnavailable } from '../../common-components/ComponentUnavailable'
-
 import { CmschPage } from '../../common-components/layout/CmschPage'
 import { PageStatus } from '../../common-components/PageStatus'
 import LeaderboardByCategoryPage from './leaderboardByCategory.page.tsx'
 import LeaderboardByUserOrGroupPage from './leaderboardByUserOrGroup.page.tsx'
+import { useMatch, useNavigate } from 'react-router-dom'
 
 const LeaderboardPage = () => {
   const component = useConfigContext()?.components.leaderboard
   const { data, isError, isLoading } = useLeaderBoardQuery(component?.leaderboardDetailsEnabled ? 'detailed' : 'short')
+  const byCategory = useMatch('/leaderboard/category')
+  const byToken = useMatch('/leaderboard/token')
+  const navigate = useNavigate()
 
   if (!component) return <ComponentUnavailable />
 
   const title = component.title || 'Toplista'
 
   if (isError || isLoading || !data) return <PageStatus isLoading={isLoading} isError={isError} title={title} />
+
+  const tabIndex = byToken ? 2 : byCategory ? 1 : 0
+
+  const handleTabChange = (i: number) => {
+    switch (i) {
+      case 0:
+        navigate('/leaderboard')
+        break
+      case 1:
+        navigate('/leaderboard/category')
+        break
+      case 2:
+        navigate('/leaderboard/token')
+        break
+    }
+  }
 
   return (
     <CmschPage disablePadding={true}>
@@ -32,7 +51,7 @@ const LeaderboardPage = () => {
         {data?.groupScore !== undefined && <BoardStat label="Csapat pont" value={data.groupScore} />}
       </HStack>
 
-      <Tabs isLazy isFitted colorScheme="brand" variant="enclosed">
+      <Tabs isLazy isFitted colorScheme="brand" variant="enclosed" index={tabIndex} onChange={handleTabChange}>
         <TabList>
           <Tab>Csapat</Tab>
           <Tab>Kategória</Tab>
