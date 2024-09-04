41ad3c2c2ba1a17825d090b0b1dcf3add933f7ab
Horváth István <horvath.isti@edu.bme.hu>
2024-09-05 00:35:25 +0200

    Fix entity configuration for TeamComponent

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/team/TeamComponentEntityConfiguration.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/team/TeamComponentEntityConfiguration.kt
@@ -1,10 +1,8 @@
 package hu.bme.sch.cmsch.component.team
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
 import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Configuration
 
 @Configuration
-@ConditionalOnBean(TeamComponent::class)
 @EntityScan(basePackageClasses = [TeamComponent::class])
 class TeamComponentEntityConfiguration
