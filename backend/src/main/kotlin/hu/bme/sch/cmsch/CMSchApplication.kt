b36df7d6437e52f5bad4de00f3ad955eb8493c9c
Horváth István <horvath.isti@edu.bme.hu>
2024-09-10 13:30:53 +0200

    Update all dependencies

--- backend/src/main/kotlin/hu/bme/sch/cmsch/CMSchApplication.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/CMSchApplication.kt
@@ -8,7 +8,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-const val CMSCH_VERSION = "4.8.0"
+const val CMSCH_VERSION = "4.8.1"
 
 @SpringBootApplication
 @EnableConfigurationProperties(value = [ComponentLoadConfig::class, StartupPropertyConfig::class])
