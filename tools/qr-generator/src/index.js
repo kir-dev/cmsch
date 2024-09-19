de37b9b34aa0f99004e7178a4044c42bf016586a
Horváth István <horvath.isti@edu.bme.hu>
2024-09-19 23:44:06 +0200

    Add print preview to qr generator

--- tools/qr-generator/src/index.js
+++ tools/qr-generator/src/index.js
@@ -3,9 +3,11 @@ import args from './args.js'
 import { stringify } from 'csv-stringify/sync'
 import { generateTokens } from './generator/token.js'
 import { generateQrCodes } from './generator/qr.js'
+import { dirname } from 'node:path'
 
 const tokens = generateTokens(args)
 const csvOptions = { header: true, cast: { boolean: value => value ? 'true' : 'false' } }
+fs.mkdirSync(dirname(args.csvOutputPath), { recursive: true })
 fs.writeFileSync(args.csvOutputPath, stringify(tokens, csvOptions))
 
 if (args.generateQrCodes) {
