4f2405f9b884f9d34ee124ae096bcdd4b04d5f9b
Horváth István <horvath.isti@edu.bme.hu>
2024-09-25 14:31:44 +0200

    Inline dynamic imports

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,13 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        inlineDynamicImports: true
+      }
+    }
+  },
   server: {
     host: '0.0.0.0',
     open: true,
