43b6934cb6612c08836131a3ed295c2abb461653
Horváth István <horvath.isti@edu.bme.hu>
2024-10-03 00:16:31 +0200

    Make overview template more robust

--- backend/src/main/kotlin/hu/bme/sch/cmsch/admin/OverviewBuilder.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/admin/OverviewBuilder.kt
@@ -1,8 +1,5 @@
 package hu.bme.sch.cmsch.admin
 
-import com.fasterxml.jackson.core.type.TypeReference
-import com.fasterxml.jackson.databind.ObjectMapper
-import hu.bme.sch.cmsch.controller.admin.ControlAction
 import java.io.ByteArrayOutputStream
 import kotlin.reflect.KClass
 import kotlin.reflect.KProperty1
@@ -15,51 +12,37 @@ class OverviewBuilder<T : Any>(val type: KClass<T>) {
 
     private fun getColumnDefinitions(): List<Pair<KProperty1<T, *>, GenerateOverview>> {
         return type.memberProperties.asSequence()
-                .filter { it.findAnnotation<GenerateOverview>() != null }
-                .map { Pair(it, it.findAnnotation<GenerateOverview>()!!) }
-                .filter { it.second.visible }
-                .sortedBy { it.second.order }
-                .toList()
+            .filter { it.findAnnotation<GenerateOverview>() != null }
+            .map { Pair(it, it.findAnnotation<GenerateOverview>()!!) }
+            .filter { it.second.visible }
+            .sortedBy { it.second.order }
+            .toList()
     }
 
-    fun getColumnsAsJson(): String {
-        return "[" + (getColumnDefinitions().joinToString(",") {
-            "{\"title\":\"${it.second.columnName}\", " +
-                    "\"field\":\"${it.first.name}\", " +
-                    "\"hozAlign\":\"${it.second.alignment()}\", " +
-                    "\"sorter\":\"${it.second.sorter()}\"" +
-                    it.second.extra() +
-                    "}"
-        }) + "]"
+    fun getColumns(): List<Map<String, Any>> = getColumnDefinitions().map {
+        mapOf(
+            "title" to it.second.columnName,
+            "field" to it.first.name,
+            "hozAlign" to it.second.alignment(),
+            "sorter" to it.second.sorter(),
+            *it.second.extra()
+        )
     }
 
-    private fun formatValue(type: GenerateOverview, value: Any?): String {
-        return if (type.renderer == OVERVIEW_TYPE_CDN_IMAGE) {
-            if (value is String && !value.isNullOrBlank())
-                "\"/cdn/${type.cdnImageFolder}/${value}\""
-            else
-                "\"data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==\"" // empty image
-        } else if (type.renderer == OVERVIEW_TYPE_TEXT || type.renderer == OVERVIEW_TYPE_ICON) {
-            "\"${(value ?: "").toString().replace("\"", "")}\""
-        } else {
-            (value ?: "0").toString().replace("\"", "")
-        }
-    }
-
-    fun getTableDataAsJson(overview: Iterable<T>): String {
+    fun getTableData(overview: Iterable<T>): List<Map<String, Any>> {
         val fields = getColumnDefinitions()
-        return "[" + (overview.toList()
-            .map { row -> fields.map { "\"${it.first.name}\":${formatValue(it.second, it.first.get(row))}" } }
-            .joinToString(",") { row -> "{${row.joinToString(",")}}" }) + "]"
+        return overview.map { row ->
+            fields.map { it.first.name to it.second.formatValue(it.first.get(row)) }.toMap()
+        }
     }
 
     fun getInputs(): List<Pair<KProperty1<out Any, *>, GenerateInput>> {
         return type.memberProperties.asSequence()
-                .filter { it.findAnnotation<GenerateInput>() != null }
-                .map { Pair(it, it.findAnnotation<GenerateInput>()!!) }
-                .filter { it.second.visible }
-                .sortedBy { it.second.order }
-                .toList()
+            .filter { it.findAnnotation<GenerateInput>() != null }
+            .map { Pair(it, it.findAnnotation<GenerateInput>()!!) }
+            .filter { it.second.visible }
+            .sortedBy { it.second.order }
+            .toList()
     }
 
     fun exportToCsv(entities: List<T>): String {
@@ -67,13 +50,6 @@ class OverviewBuilder<T : Any>(val type: KClass<T>) {
         csv.exportToCsv(entities, outputStream)
         return outputStream.toString().trim()
     }
-
-    fun toJson(list: List<ControlAction>, objectMapper: ObjectMapper): String {
-        return objectMapper
-            .writerFor(object : TypeReference<List<ControlAction>>() {})
-            .writeValueAsString(list)
-    }
-
 }
 
 object DetailsHelper {
