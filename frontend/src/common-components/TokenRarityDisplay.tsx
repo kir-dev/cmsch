2eec9bf99613aa195540df73a835ada0a44047fb
Horváth István <horvath.isti@edu.bme.hu>
2024-10-02 15:59:52 +0200

    Remove max count from token rarity display

--- frontend/src/common-components/TokenRarityDisplay.tsx
+++ frontend/src/common-components/TokenRarityDisplay.tsx
@@ -1,5 +1,38 @@
 import { Box, Flex, Text } from '@chakra-ui/react'
-import { useConfigContext } from '../api/contexts/config/ConfigContext.tsx'
+
+const rarities = [
+  {
+    rarity: 'COMMON',
+    color: '#b7bfc5',
+    background: '#40464d'
+  },
+  {
+    rarity: 'UNCOMMON',
+    color: '#61bf00',
+    background: '#024f03'
+  },
+  {
+    rarity: 'RARE',
+    color: '#00afff',
+    background: '#00458a'
+  },
+  {
+    rarity: 'EPIC',
+    color: '#ce59ff',
+    background: '#4c197b'
+  },
+  {
+    rarity: 'LEGENDARY',
+    color: '#ffe8cf',
+    background: '#de6e0e'
+  },
+  {
+    rarity: 'RAINBOW',
+    color: '#000',
+    background:
+      'linear-gradient(45deg, #ff0000, #ff8000, #ffff00, #80ff00, #00ff00, #00ff80, #00ffff, #0080ff, #0000ff, #8000ff, #ff00ff, #ff0080)'
+  }
+]
 
 type TokenRarityChipProps = {
   count: number
@@ -10,8 +43,7 @@ type TokenRarityChipProps = {
 }
 
 // based on https://fortnite.fandom.com/wiki/Rarity
-const TokenRarityChip = ({ count, max, color, background, showMax }: TokenRarityChipProps) => {
-  const unique = max == 1
+const TokenRarityChip = ({ count, color, background }: TokenRarityChipProps) => {
   return (
     <Box
       display="inline-block"
@@ -26,13 +58,8 @@ const TokenRarityChip = ({ count, max, color, background, showMax }: TokenRarity
       fontSize="1.1em"
     >
       <Text as="span" fontWeight="900">
-        {unique ? 'LGBTQ' : count}
+        {count}
       </Text>
-      {showMax && max && !unique && (
-        <Text fontSize=".7em" as="sub">
-          /{max}
-        </Text>
-      )}
     </Box>
   )
 }
@@ -42,64 +69,12 @@ interface TokenRarityDisplayProps {
 }
 
 export const TokenRarityDisplay = ({ collected }: TokenRarityDisplayProps) => {
-  const showMaxCount = useConfigContext().components.leaderboard.showTokenMaxCountByRarity
-
-  // TODO: get count by rarity from config
-  const maxByRarity: { [key: string]: number } = {
-    COMMON: 1500,
-    UNCOMMON: 500,
-    RARE: 250,
-    EPIC: 125,
-    LEGENDARY: 62,
-    RAINBOW: 1
-  }
-
-  const rarities = [
-    {
-      rarity: 'COMMON',
-      color: '#b7bfc5',
-      background: '#40464d'
-    },
-    {
-      rarity: 'UNCOMMON',
-      color: '#61bf00',
-      background: '#024f03'
-    },
-    {
-      rarity: 'RARE',
-      color: '#00afff',
-      background: '#00458a'
-    },
-    {
-      rarity: 'EPIC',
-      color: '#ce59ff',
-      background: '#4c197b'
-    },
-    {
-      rarity: 'LEGENDARY',
-      color: '#ffe8cf',
-      background: '#de6e0e'
-    },
-    {
-      rarity: 'RAINBOW',
-      color: '#000',
-      background: 'linear-gradient(45deg, #ff0000, #ff8000, #ffff00, #80ff00, #00ff00, #00ff80, #00ffff, #0080ff, #0000ff, #8000ff, #ff00ff, #ff0080)'
-    }
-  ]
-
   return (
     <Flex gap={1} wrap="wrap">
       {rarities
         .filter((rarity) => collected[rarity.rarity] > 0)
         .map((rarity) => (
-          <TokenRarityChip
-            key={rarity.rarity}
-            count={collected[rarity.rarity]}
-            max={maxByRarity[rarity.rarity]}
-            color={rarity.color}
-            background={rarity.background}
-            showMax={showMaxCount}
-          />
+          <TokenRarityChip key={rarity.rarity} count={collected[rarity.rarity]} color={rarity.color} background={rarity.background} />
         ))}
     </Flex>
   )
