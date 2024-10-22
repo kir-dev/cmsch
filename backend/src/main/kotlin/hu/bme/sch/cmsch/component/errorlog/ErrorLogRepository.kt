5e910d4afcf82f4bfc2988a8acb76597d019b894
Horváth István <horvath.isti@edu.bme.hu>
2024-10-22 15:55:29 +0200

    Add error reporting

---
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/errorlog/ErrorLogRepository.kt
@@ -0,0 +1,26 @@
+package hu.bme.sch.cmsch.component.errorlog
+
+import hu.bme.sch.cmsch.model.RoleType
+import hu.bme.sch.cmsch.repository.EntityPageDataSource
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+import java.util.Optional
+
+@Repository
+@ConditionalOnBean(ErrorLogComponent::class)
+interface ErrorLogRepository : CrudRepository<ErrorLogEntity, Int>, EntityPageDataSource<ErrorLogEntity, Int> {
+
+    @Query("select e from ErrorLogEntity e order by e.lastReportedAt desc")
+    override fun findAll(): List<ErrorLogEntity>
+
+    fun findByMessageAndStackAndUserAgentAndHrefAndRole(
+        message: String,
+        stack: String,
+        userAgent: String,
+        href: String,
+        role: RoleType
+    ): Optional<ErrorLogEntity>
+
+}
