050e4d05c0ac00017c5f9bd3c0ce0b7c6d0c6b3a
Szabó Benedek <benedek.szabo2003@gmail.com>
2025-01-30 22:38:03 +0100

    tournament: Component Settings fixed, some reformat, new attributes

--- backend/src/main/kotlin/hu/bme/sch/cmsch/component/tournament/TournamentComponent.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/component/tournament/TournamentComponent.kt
@@ -29,11 +29,18 @@ class TournamentComponent (
 ){
     final override val allSettings by lazy {
         listOf(
+            tournamentGroup,
             minRole,
         )
     }
 
     final override val menuDisplayName = null
+
+    final val tournamentGroup = SettingProxy(componentSettingService, component,
+        "tournamentGroup", "", type = SettingType.COMPONENT_GROUP, persist = false,
+        fieldName = "Tournament",
+        description = "Jelenleg nincs mit beállítani itt"
+    )
 
     final override val minRole = MinRoleSettingProxy(componentSettingService, component,
         "minRole", MinRoleSettingProxy.ALL_ROLES, minRoleToEdit = RoleType.ADMIN,
