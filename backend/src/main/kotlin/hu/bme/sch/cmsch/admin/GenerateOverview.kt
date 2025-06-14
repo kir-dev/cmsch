e7aca0850f9257d017bb20d7fbc4d3cd37e668ae
Horváth István <horvath.isti@edu.bme.hu>
2025-06-14 12:37:20 +0200

    Add color input

--- backend/src/main/kotlin/hu/bme/sch/cmsch/admin/GenerateOverview.kt
+++ backend/src/main/kotlin/hu/bme/sch/cmsch/admin/GenerateOverview.kt
@@ -4,24 +4,15 @@ package hu.bme.sch.cmsch.admin
  * For sorters: https://tabulator.info/docs/5.4/sort#func-builtin
  */
 enum class OverviewType(val value: String, val sorter: String, val formatSettings: Array<Pair<String, Any>>) {
-    ID("id", "number",
-        arrayOf("width" to 100, "vertAlign" to "middle", "visible" to false)),
-    TEXT("text", "string",
-        arrayOf("vertAlign" to "middle")),
-    DATE("date", "datetime",
-        arrayOf("vertAlign" to "middle", "formatter" to "datetime")),
-    COLOR("color", "string",
-        arrayOf("formatter" to "color", "vertAlign" to "middle")),
-    BOOLEAN("boolean", "boolean",
-        arrayOf("formatter" to "tickCross", "width" to 120)),
-    ICON("icon", "string",
-        arrayOf("vertAlign" to "middle", "formatter" to "enumIconsFormatter", "width" to 120)),
-    TIME("time", "time",
-        arrayOf("vertAlign" to "middle")),
-    NUMBER("number", "number",
-        arrayOf("vertAlign" to "middle")),
-    IMAGE("image", "string",
-        arrayOf("formatter" to "image", "width" to 120, "formatterParams" to mapOf("height" to "100px")));
+    ID("id", "number", arrayOf("width" to 100, "vertAlign" to "middle", "visible" to false)),
+    TEXT("text", "string", arrayOf("vertAlign" to "middle")),
+    DATE("date", "datetime", arrayOf("vertAlign" to "middle", "formatter" to "datetime")),
+    COLOR("color", "string", arrayOf("formatter" to "color", "width" to 20)),
+    BOOLEAN("boolean", "boolean", arrayOf("formatter" to "tickCross", "width" to 120)),
+    ICON("icon", "string", arrayOf("vertAlign" to "middle", "formatter" to "enumIconsFormatter", "width" to 120)),
+    TIME("time", "time", arrayOf("vertAlign" to "middle")),
+    NUMBER("number", "number", arrayOf("vertAlign" to "middle")),
+    IMAGE("image", "string", arrayOf("formatter" to "image", "width" to 120, "formatterParams" to mapOf("height" to "100px")));
 }
 
 
