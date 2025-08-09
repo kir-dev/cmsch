89143247dd56204a462a23431aa2736b5dd9b92b
Szabó Benedek <benedek.szabo2003@gmail.com>
2025-08-09 14:32:41 +0200

    sonar codesmell

--- frontend/src/pages/tournament/components/KnockoutBracket.tsx
+++ frontend/src/pages/tournament/components/KnockoutBracket.tsx
@@ -8,41 +8,31 @@ interface KnockoutBracketProps {
 
 const KnockoutBracket: React.FC<KnockoutBracketProps> = ({ tree }: KnockoutBracketProps) => {
   return (
-    <>
-      <Stack direction="row" position="relative">
-        {(tree.upperTree || tree.lowerTree) && (
-          <>
-            <Stack direction="column" align="center" mr={-2} mb={2}>
-              {tree.upperTree && <KnockoutBracket tree={tree.upperTree} />}
-              {tree.lowerTree && <KnockoutBracket tree={tree.lowerTree} />}
-            </Stack>
-            <Box width="40" mr={-2}>
-              {tree.upperTree && tree.lowerTree && (
-                <>
-                  <Box top="25%" left="0" position="relative" width="50%" height="2px" bg="gray.300" />
-                  <Box top="75%" left="0" position="relative" width="50%" height="2px" bg="gray.300" />
-                  <Box top="50%" left="50%" position="relative" width="50%" height="2px" bg="gray.300" />
-                  <Box top="22.5%" left="50%" position="relative" width="2px" height="51.5%" bg="gray.300" />
-                </>
-              )}
-              {tree.upperTree && !tree.lowerTree && (
-                <>
-                  <Box top="50%" left="0" position="relative" width="100%" height="2px" bg="gray.300" />
-                </>
-              )}
-              {!tree.upperTree && tree.lowerTree && (
-                <>
-                  <Box top="50%" left="0" position="relative" width="100%" height="2px" bg="gray.300" />
-                </>
-              )}
-            </Box>
-          </>
-        )}
-        <Box alignContent="center">
-          <Match match={tree.root} />
-        </Box>
-      </Stack>
-    </>
+    <Stack direction="row" position="relative">
+      {(tree.upperTree || tree.lowerTree) && (
+        <>
+          <Stack direction="column" align="center" mr={-2} mb={2}>
+            {tree.upperTree && <KnockoutBracket tree={tree.upperTree} />}
+            {tree.lowerTree && <KnockoutBracket tree={tree.lowerTree} />}
+          </Stack>
+          <Box width="40" mr={-2}>
+            {tree.upperTree && tree.lowerTree && (
+              <>
+                <Box top="25%" left="0" position="relative" width="50%" height="2px" bg="gray.300" />
+                <Box top="75%" left="0" position="relative" width="50%" height="2px" bg="gray.300" />
+                <Box top="50%" left="50%" position="relative" width="50%" height="2px" bg="gray.300" />
+                <Box top="22.5%" left="50%" position="relative" width="2px" height="51.5%" bg="gray.300" />
+              </>
+            )}
+            {tree.upperTree && !tree.lowerTree && <Box top="50%" left="0" position="relative" width="100%" height="2px" bg="gray.300" />}
+            {!tree.upperTree && tree.lowerTree && <Box top="50%" left="0" position="relative" width="100%" height="2px" bg="gray.300" />}
+          </Box>
+        </>
+      )}
+      <Box alignContent="center">
+        <Match match={tree.root} />
+      </Box>
+    </Stack>
   )
 }
 
