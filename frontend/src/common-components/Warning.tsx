33a74b44ff2f8801add0c8a2e426f85887fb20e6
Panny2507 <sajbananna2005@gmail.com>
2025-08-29 22:09:32 +0200

    remove warning endpoint (#938)

--- frontend/src/common-components/Warning.tsx
+++ frontend/src/common-components/Warning.tsx
@@ -1,18 +1,20 @@
 import { Alert, AlertDescription, AlertIcon, Box, CloseButton, HStack, useDisclosure, VStack } from '@chakra-ui/react'
-import { useWarningQuery } from '../api/hooks/warning/useWarning'
+import { useConfigContext } from '../api/contexts/config/ConfigContext.tsx'
 
 export const Warning = () => {
   const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true })
-  const { data, error } = useWarningQuery()
+  const config = useConfigContext()
+  const message = config?.components?.app?.warningMessage
+  const level = config?.components?.app?.warningLevel
 
-  if (!data || error || !isOpen || !data.message) return null
+  if (!isOpen || !message) return null
 
   return (
     <Box>
       <Alert
         borderRadius={[0, null, 'xl']}
         opacity={1}
-        status={data.type || 'warning'}
+        status={level || 'warning'}
         variant="solid"
         mx="auto"
         w="100%"
@@ -22,7 +24,7 @@ export const Warning = () => {
         <HStack justify="space-between" flex={1}>
           <AlertIcon />
           <VStack align="flex-start" flex={1}>
-            <AlertDescription wordBreak="break-word">{data.message}</AlertDescription>
+            <AlertDescription wordBreak="break-word">{message}</AlertDescription>
           </VStack>
           <CloseButton onClick={onClose} />
         </HStack>
