3672f5d5ffa43f16e94e23240446c3fc62c13d0f
Horváth István <horvath.isti@edu.bme.hu>
2025-08-30 12:25:48 +0200

    Use npm (finally)

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,7 +7,7 @@ import globals from 'globals'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config([
-  globalIgnores(['dist', '.yarn']),
+  globalIgnores(['dist']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
