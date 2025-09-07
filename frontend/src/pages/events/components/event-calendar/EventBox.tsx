088257286ccaf19ee5bb562355848f1c95b3963c
Albert Ragány-Németh <me@alb1.hu>
2025-09-07 13:13:44 +0200

    Add support for overlapping events in the calendar UI
    AI (Gemini CLI) was used in the making of this change.

--- frontend/src/pages/events/components/event-calendar/EventBox.tsx
+++ frontend/src/pages/events/components/event-calendar/EventBox.tsx
@@ -18,7 +18,7 @@ import { formatHu, stringifyTimeRange } from '../../../../util/core-functions.ut
 import { AbsolutePaths } from '../../../../util/paths'
 import { EventListView } from '../../../../util/views/event.view'
 
-export type EventBoxItem = EventListView & { top: number; bottom: number; conflictingEventsBefore?: number }
+export type EventBoxItem = EventListView & { top: number; bottom: number; width: number; left: number }
 
 interface EventBoxProps {
   boxRef?: RefObject<HTMLDivElement>
@@ -33,13 +33,12 @@ export function EventBox({ event, boxRef }: EventBoxProps) {
     <Popover>
       <PopoverTrigger>
         <Box
-          ml={(event.conflictingEventsBefore ?? 0) * 2}
           ref={boxRef}
           overflow="hidden"
           key={event.url}
           position="absolute"
-          left={0}
-          right={0}
+          left={event.left + '%'}
+          width={event.width + '%'}
           top={event.top + '%'}
           bottom={event.bottom + '%'}
           bg={eventBg}
