3a998a8d85c7e9a7551b51ee2a56cbbf9d527336
Balog Bors <144214912+balog-b@users.noreply.github.com>
2025-09-07 18:43:28 +0200

    Feature/labels (#946)
    * feat: team label entity and view
    
    * feat: color picker & luminance based color alternation
    
    * fix: darker alt color on light color
    
    * fix: color picker desc & label+tooltip composition

---
+++ frontend/src/pages/teams/components/TeamLabel.tsx
@@ -0,0 +1,47 @@
+import { Box, Tooltip } from '@chakra-ui/react'
+import { ColorInstance } from 'color'
+import { useAltColor, useColor } from '../../../util/color.utils.ts'
+import { TeamLabelView } from '../../../util/views/team.view.ts'
+
+const TeamLabel = ({ label }: { label: TeamLabelView }) => {
+  const myColor: ColorInstance = useColor(label.color)
+
+  const [colorHex, darkColor] = [myColor.hex(), useAltColor(myColor).hex()]
+
+  return label.desc ? (
+    <LabelWithTooltip name={label.name} color={colorHex} darkColor={darkColor} desc={label.desc} />
+  ) : (
+    <LabelComponent name={label.name} color={colorHex} darkColor={darkColor} />
+  )
+}
+
+const LabelComponent = ({ name, color, darkColor }: { name: string; color: string; darkColor: string }) => {
+  return (
+    <Box
+      border="2px solid"
+      bg={darkColor}
+      borderColor={color}
+      color={color}
+      px={3}
+      py={0.5}
+      borderRadius="xl"
+      fontWeight="bold"
+      fontSize={12}
+      letterSpacing={1.2}
+    >
+      {name}
+    </Box>
+  )
+}
+
+const LabelWithTooltip = ({ name, color, darkColor, desc }: { name: string; color: string; darkColor: string; desc: string }) => {
+  return (
+    <Tooltip label={desc} aria-label={desc}>
+      <Box>
+        <LabelComponent name={name} color={color} darkColor={darkColor} />
+      </Box>
+    </Tooltip>
+  )
+}
+
+export default TeamLabel
