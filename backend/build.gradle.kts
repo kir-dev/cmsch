979603475e61c332baa4662a8ab87adba9e8b8d3
Horváth István <horvath.isti@edu.bme.hu>
2026-04-26 21:27:53 +0200

    Update backend dependencies

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -3,11 +3,11 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "4.0.4"
+    id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
-    id("org.owasp.dependencycheck") version "12.2.0"
-    kotlin("jvm") version "2.3.20"
-    kotlin("plugin.spring") version "2.3.20"
+    id("org.owasp.dependencycheck") version "12.2.1"
+    kotlin("jvm") version "2.3.21"
+    kotlin("plugin.spring") version "2.3.21"
     id("org.sonarqube") version "7.2.3.7755"
 }
 
@@ -52,21 +52,21 @@ dependencies {
     implementation("tools.jackson.dataformat:jackson-dataformat-csv")
     implementation("tools.jackson.module:jackson-module-kotlin")
     implementation("com.github.spullara.mustache.java:compiler:0.9.14")
-    implementation("com.google.auth:google-auth-library-oauth2-http:1.43.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.46.0")
     implementation("com.google.zxing:core:3.5.4")
     implementation("com.google.zxing:javase:3.5.4")
-    implementation("com.itextpdf:itext-core:9.5.0")
+    implementation("com.itextpdf:itext-core:9.6.0")
     implementation("com.squareup.okhttp3:okhttp:5.3.2")
     implementation(platform("io.jsonwebtoken:jjwt-bom:0.13.0"))
     runtimeOnly("io.jsonwebtoken:jjwt-impl")
     runtimeOnly("io.jsonwebtoken:jjwt-jackson")
     implementation("io.jsonwebtoken:jjwt-api")
-    implementation(platform("io.micrometer:micrometer-bom:1.16.4"))
+    implementation(platform("io.micrometer:micrometer-bom:1.16."))
     runtimeOnly("io.micrometer:micrometer-core")
     runtimeOnly("io.micrometer:micrometer-observation")
     runtimeOnly("io.micrometer:micrometer-registry-prometheus")
-    implementation("org.commonmark:commonmark-ext-gfm-tables:0.27.1")
-    implementation("org.commonmark:commonmark:0.27.1")
+    implementation("org.commonmark:commonmark-ext-gfm-tables:0.28.0")
+    implementation("org.commonmark:commonmark:0.28.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-scripting-common")
     implementation("org.jetbrains.kotlin:kotlin-scripting-dependencies")
@@ -75,7 +75,7 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-scripting-jvm-host")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.2")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:3.0.3")
     implementation("org.springframework.boot:spring-boot-starter-actuator")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
@@ -85,7 +85,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-webclient")
     implementation("org.springframework.session:spring-session-jdbc")
-    implementation("software.amazon.awssdk:s3:2.42.18")
+    implementation("software.amazon.awssdk:s3:2.42.40")
     runtimeOnly("com.h2database:h2")
     runtimeOnly("org.postgresql:postgresql")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
