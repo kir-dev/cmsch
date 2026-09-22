56c2e469b0f09cd1fcb6bdff8200c578b04e527d
Isti01 <horvath.isti@edu.bme.hu>
2026-09-23 00:55:40 +0200

    Frontend distribution improvements (#1071)
    * Enable sourcemaps
    
    * Enable caching in nginx

--- frontend/src/common-components/footer/PartnerLogo.tsx
+++ frontend/src/common-components/footer/PartnerLogo.tsx
@@ -1,4 +1,19 @@
+import bmeDark from '@/assets/img/supporters/bme_dark.svg'
+import bmeLight from '@/assets/img/supporters/bme_light.svg'
+import schdesignDark from '@/assets/img/supporters/schdesign_dark.svg'
+import schdesignLight from '@/assets/img/supporters/schdesign_light.svg'
+import schonherzDark from '@/assets/img/supporters/schonherz_dark.svg'
+import schonherzLight from '@/assets/img/supporters/schonherz_light.svg'
+import vikDark from '@/assets/img/supporters/vik_dark.svg'
+import vikLight from '@/assets/img/supporters/vik_light.svg'
 import { useColorModeValue } from '@/util/core-functions.util'
+
+const partnerLogos = {
+  bme: { light: bmeLight, dark: bmeDark },
+  schonherz: { light: schonherzLight, dark: schonherzDark },
+  schdesign: { light: schdesignLight, dark: schdesignDark },
+  vik: { light: vikLight, dark: vikDark }
+} as const
 
 interface PartnerLogoProps {
   name: 'bme' | 'schonherz' | 'schdesign' | 'vik'
@@ -6,6 +21,5 @@ interface PartnerLogoProps {
 
 export function PartnerLogo({ name }: PartnerLogoProps) {
   const fileVariant = useColorModeValue('light', 'dark')
-  const fileName = `${name}_${fileVariant}`
-  return <img className="m-5 max-h-20 max-w-32 object-contain" src={`/img/supporters/${fileName}.svg`} alt={name} />
+  return <img className="m-5 max-h-20 max-w-32 object-contain" src={partnerLogos[name][fileVariant]} alt={name} />
 }
